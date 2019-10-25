3d5ff79f7159d42f98bae719101163470f59176c
hirsch <gery.hirschfeld@w3tec.ch>
2019-10-25 11:14:04 +0200

    chore: use ssr for deploy

---
+++ ssr.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  template: "./ssr.html",
+  maxAge: 60 * 60 * 1000,
+  config: {
+    name: "Bal UI Library",
+    repo: "https://github.com/hirsch88/bal-ui-library",
+    coverpage: true,
+    loadSidebar: true,
+    subMaxLevel: 1,
+    themeColor: "#009fee",
+    auto2top: true,
+    homepage: "home.md",
+    executeScript: true,
+  },
+};
