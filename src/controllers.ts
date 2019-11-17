732c72232a4d34e9e15460b26624ca66653f28bf
hirsch <gery.hirschfeld@w3tec.ch>
2019-11-17 20:56:53 +0100

    fix(controllers): correct global script content

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,3 +1,17 @@
 import {balToastController} from "./components/bal-toast/bal-toast.controller";
 
-(window as any).balToastController = balToastController;
+/**
+ * Global Script
+ * -------------------
+ * The global script runs once before your library/app loads, so you can do things like setting up a connection
+ * to an external service or configuring a library you are using.
+ *
+ * https://stenciljs.com/docs/config#globalscript
+ */
+
+export default function main() {
+  /**
+   * Place your controllers here ...
+   */
+  (window as any).balToastController = balToastController;
+}
