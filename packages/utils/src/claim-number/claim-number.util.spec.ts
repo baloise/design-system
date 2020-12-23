de8c21d55c9641dc1ff89192011c6bd7bdaf9903
hirsch <gery.hirschfeld@w3tec.ch>
2020-12-23 08:05:50 +0100

    fix(utils): refactor utils

--- packages/utils/src/claim-number/claim-number.util.spec.ts
+++ packages/utils/src/claim-number/claim-number.util.spec.ts
@@ -1,16 +1,16 @@
-import { claimNumber } from './claim-number.util'
+import { toClaimNumber } from './claim-number.util'
 
 describe('ClaimNumberUtils', () => {
   test('should format a claim number correctly', () => {
-    expect(claimNumber.format('73001217169')).toBe('73/001217/16.9')
+    expect(toClaimNumber('73001217169')).toBe('73/001217/16.9')
   })
   test('should format null as empty string', () => {
-    expect(claimNumber.format(null)).toBe('')
+    expect(toClaimNumber(null)).toBe('')
   })
   test('should output a number with a wrong format raw', () => {
-    expect(claimNumber.format('123')).toBe('123')
+    expect(toClaimNumber('123')).toBe('123')
   })
   test('should format a claim number with a sign postfix correctly', () => {
-    expect(claimNumber.format('7300772816X')).toBe('73/007728/16.X')
+    expect(toClaimNumber('7300772816X')).toBe('73/007728/16.X')
   })
 })
--- packages/utils/src/claim-number/claim-number.util.ts
+++ packages/utils/src/claim-number/claim-number.util.ts
@@ -1,17 +1,13 @@
-class ClaimNumberUtils {
-  /**
-   * Formats the claim-number correctly.
-   */
-  format(claimNumber: string): string {
-    if (!claimNumber) {
-      return ''
-    }
-    const parts = claimNumber.match(/^(\d{2})(\d{6})(\d{2})(\w{1})$/)
-    if (!parts) {
-      return claimNumber
-    }
-    return `${parts[1]}/${parts[2]}/${parts[3]}.${parts[4]}`
+/**
+ * Formats the claim-number correctly.
+ */
+export const toClaimNumber = (claimNumber: string) => {
+  if (!claimNumber) {
+    return ''
   }
+  const parts = claimNumber.match(/^(\d{2})(\d{6})(\d{2})(\w{1})$/)
+  if (!parts) {
+    return claimNumber
+  }
+  return `${parts[1]}/${parts[2]}/${parts[3]}.${parts[4]}`
 }
-
-export const claimNumber = new ClaimNumberUtils()
