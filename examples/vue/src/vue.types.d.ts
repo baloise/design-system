09551b824fc02a2e4faf71a22d3c57f833891d08
hirsch <gery.hirschfeld@w3tec.ch>
2021-01-26 08:23:36 +0100

    test(datepicker): add pointer events to clickable tags

--- examples/vue/src/vue.types.d.ts
+++ examples/vue/src/vue.types.d.ts
@@ -1,17 +1,1 @@
-// import Vue from 'vue'
-// import { BalUtilsStatic } from '@baloise/ui-library-utils'
-// import { BalToastController, BalSnackbarController } from '@baloise/ui-library'
-
-// declare module 'vue/types/vue' {
-//   interface VueConstructor {
-//     $balUtils: BalUtilsStatic
-//     $balToast: BalToastController
-//     $balSnackbar: BalSnackbarController
-//   }
-
-//   interface Vue {
-//     $balUtils: BalUtilsStatic
-//     $balToast: BalToastController
-//     $balSnackbar: BalSnackbarController
-//   }
-// }
+import '@baloise/ui-library-vue/types'
