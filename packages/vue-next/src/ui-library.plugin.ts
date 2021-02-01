4aa7951e0c9db6d8b8765b08e5a1e7074e0c5d62
hirsch <gery.hirschfeld@w3tec.ch>
2021-02-01 21:17:12 +0100

    feat(vue): add vue 3.x.x support

--- packages/vue-next/src/ui-library.plugin.ts
+++ packages/vue-next/src/ui-library.plugin.ts
@@ -1,16 +1,26 @@
 import { Plugin } from 'vue'
 import { defineCustomElements, applyPolyfills } from '@baloise/ui-library/loader'
-import { balToastController, balSnackbarController } from '@baloise/ui-library'
+import {
+  balToastController,
+  balSnackbarController,
+  BalToastController,
+  BalSnackbarController,
+} from '@baloise/ui-library'
 import * as balUtils from '@baloise/ui-library-utils'
 
 export const BaloiseUILibrary: Plugin = {
   async install(app) {
     await applyPolyfills()
     await defineCustomElements()
+
     app.config.isCustomElement = tag => tag.startsWith('bal-')
 
     app.config.globalProperties.$balUtils = balUtils
     app.config.globalProperties.$balToast = balToastController
     app.config.globalProperties.$balSnackbar = balSnackbarController
+
+    app.provide<typeof balUtils>('balUtils', balUtils)
+    app.provide<BalToastController>('balToast', balToastController)
+    app.provide<BalSnackbarController>('balSnackbar', balSnackbarController)
   },
 }
