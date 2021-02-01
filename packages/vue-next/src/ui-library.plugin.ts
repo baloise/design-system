8b4e47b63f6d85ec9221033ca4bc58eace0ce8ff
hirsch <gery.hirschfeld@w3tec.ch>
2021-02-01 20:40:59 +0100

    feat(vue): add vue 3.x.x support

--- packages/vue-next/src/ui-library.plugin.ts
+++ packages/vue-next/src/ui-library.plugin.ts
@@ -9,31 +9,8 @@ export const BaloiseUILibrary: Plugin = {
     await defineCustomElements()
     app.config.isCustomElement = tag => tag.startsWith('bal-')
 
-    app.config.globalProperties.$balFilter = balFilters
     app.config.globalProperties.$balUtils = balUtils
     app.config.globalProperties.$balToast = balToastController
     app.config.globalProperties.$balSnackbar = balSnackbarController
   },
 }
-
-// export const BalUiLibraryPlugin: PluginObject<BalUiLibraryPluginOption> = {
-//   install(_VueInstance, options): void {
-//     options = {
-//       ...BalUiLibraryPluginOptionDefaults,
-//       ...options,
-//     }
-
-//     if (options.defineCustomeElements === true) {
-//       applyPolyfills().then(() => defineCustomElements())
-//     }
-
-//     ;(_VueInstance as any).$balUtils = balUtils
-//     _VueInstance.prototype.$balUtils = balUtils
-//     ;(_VueInstance as any).$balToast = balToastController
-//     _VueInstance.prototype.$balToast = balToastController
-//     ;(_VueInstance as any).$balSnackbar = balSnackbarController
-//     _VueInstance.prototype.$balSnackbar = balSnackbarController
-
-//     addFilters(_VueInstance)
-//   },
-// }
