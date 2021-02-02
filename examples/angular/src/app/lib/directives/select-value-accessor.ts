32cabaa4e891211234faf885c446476ab6549c2a
hirsch <gery.hirschfeld@w3tec.ch>
2021-02-02 17:53:48 +0100

    feat(angular): change output types and return custom event detail

---
+++ examples/angular/src/app/lib/directives/select-value-accessor.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { ValueAccessor } from './value-accessor';
+
+@Directive({
+  /* tslint:disable-next-line:directive-selector */
+  selector: 'bal-radio-group, bal-select, bal-datepicker, bal-timeinput',
+  host: {
+    '(balChange)': 'handleChangeEvent($event.target.value)'
+  },
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: SelectValueAccessor,
+      multi: true
+    }
+  ]
+})
+export class SelectValueAccessor extends ValueAccessor {
+  constructor(el: ElementRef) {
+    super(el);
+  }
+}
