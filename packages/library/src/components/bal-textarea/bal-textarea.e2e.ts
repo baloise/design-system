f74f8d14e78b3c14a18b66dafee9ccd8132c45e2
hirsch <gery.hirschfeld@w3tec.ch>
2021-02-16 08:06:46 +0100

    test(textarea): add test for value changes and events

--- packages/library/src/components/bal-textarea/bal-textarea.e2e.ts
+++ packages/library/src/components/bal-textarea/bal-textarea.e2e.ts
@@ -61,45 +61,46 @@ describe('bal-textarea', () => {
     expect(balChangeEvent).toHaveReceivedEvent()
   })
 
-  // it('should fire no balChange and no balInput, beacuse the field has still a focus', async () => {
-  //   const page = await newE2EPage()
-  //   await page.setContent(`<bal-textarea></bal-textarea>`)
-  //   const balChangeEvent = await page.spyOnEvent('balChange')
-  //   const balInputEvent = await page.spyOnEvent('balInput')
-  //   const balInputElement = await page.find('bal-textarea')
-  //   const inputElement = await balInputElement.find('textarea')
+  it('should fire no balChange and no balInput, beacuse the field has still a focus', async () => {
+    const page = await newE2EPage()
+    await page.setContent(`<bal-textarea></bal-textarea>`)
+    const balChangeEvent = await page.spyOnEvent('balChange')
+    const balInputEvent = await page.spyOnEvent('balInput')
+    const balInputElement = await page.find('bal-textarea')
+    const inputElement = await balInputElement.find('textarea')
 
-  //   const value = await balInputElement.getProperty('value')
-  //   expect(value).toBe('')
+    const value = await balInputElement.getProperty('value')
+    expect(value).toBe('')
 
-  //   inputElement.focus()
+    inputElement.focus()
 
-  //   await balInputElement.setProperty('value', '88')
-  //   await page.waitForChanges()
+    await balInputElement.setProperty('value', '88')
+    await page.waitForChanges()
 
-  //   expect(balInputEvent).not.toHaveReceivedEvent()
-  //   expect(balChangeEvent).not.toHaveReceivedEvent()
-  // })
-  // it('should fire no balChange and no balInput, beacuse the field has still a focus', async () => {
-  //   const page = await newE2EPage()
-  //   await page.setContent(`<bal-input></bal-input>`)
-  //   const balChangeEvent = await page.spyOnEvent('balChange')
-  //   const balInputEvent = await page.spyOnEvent('balInput')
-  //   const balInputElement = await page.find('bal-textarea')
-  //   const inputElement = await balInputElement.find('textarea')
+    expect(balInputEvent).not.toHaveReceivedEvent()
+    expect(balChangeEvent).not.toHaveReceivedEvent()
+  })
 
-  //   const value = await balInputElement.getProperty('value')
-  //   expect(value).toBe('')
+  it('should fire no balChange and no balInput, beacuse the field has still a focus', async () => {
+    const page = await newE2EPage()
+    await page.setContent(`<bal-textarea></bal-textarea>`)
+    const balChangeEvent = await page.spyOnEvent('balChange')
+    const balInputEvent = await page.spyOnEvent('balInput')
+    const balInputElement = await page.find('bal-textarea')
+    const textarea = await balInputElement.find('textarea')
 
-  //   inputElement.focus()
+    const value = await textarea.getProperty('value')
+    expect(value).toBe('')
 
-  //   await balInputElement.setProperty('value', '88')
-  //   await page.waitForChanges()
+    textarea.focus()
 
-  //   await inputElement.press('Tab')
-  //   await page.waitForChanges()
+    await balInputElement.setProperty('value', '88')
+    await page.waitForChanges()
 
-  //   expect(balInputEvent).not.toHaveReceivedEvent()
-  //   expect(balChangeEvent).toHaveReceivedEvent()
-  // })
+    await textarea.press('Tab')
+    await page.waitForChanges()
+
+    expect(balInputEvent).not.toHaveReceivedEvent()
+    expect(balChangeEvent).toHaveReceivedEvent()
+  })
 })
