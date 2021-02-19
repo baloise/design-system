7119726c313dae477e5e8ca4ab3e613d0ef2d2a3
hirsch88 <gery.hirschfeld@w3tec.ch>
2021-02-19 18:18:42 +0100

    fix(ie): change target to es5

--- examples/angular/src/polyfills.ts
+++ examples/angular/src/polyfills.ts
@@ -17,18 +17,19 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
+import 'core-js'
 
 /**
  * IE11 requires the following for NgClass support on SVG elements
  */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'classlist.js' // Run `npm install --save classlist.js`.
 
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js' // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
@@ -57,8 +58,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone' // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
