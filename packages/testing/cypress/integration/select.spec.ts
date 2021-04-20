3baec724958a3d712d8a6e994f83dc7ae135d397
hirsch <gery.hirschfeld@w3tec.ch>
2021-04-20 13:06:52 +0200

    test(select): fix test

--- packages/testing/cypress/integration/select.spec.ts
+++ packages/testing/cypress/integration/select.spec.ts
@@ -1,20 +1,20 @@
 import { app } from '../support/app'
 
-describe("Select", () => {
+describe('Select', () => {
   let page = app.getSelectPage()
 
-  it("should navigate to Select page and select first option", () => {
+  it('should navigate to Select page and select first option', () => {
     page.open()
     page.select.get().click()
     page.select.get().select(0)
   })
 
-  it("should navigate to Select page and assert values in Select", () => {
+  it('should navigate to Select page and assert values in Select', () => {
     page.open()
-    page.select.get().assertOptions('1995', '1996', '1997', '1998', '1999', '2000')
+    page.select.get().assertOptions('v1995', 'v1996', 'v1997', 'v1998', 'v1999', 'v2000')
   })
 
-  it("should navigate to Select page and check if input contains value 1995", () => {
+  it('should navigate to Select page and check if input contains value 1995', () => {
     page.open()
     page.select.get().click()
     page.select.get().select(0)
