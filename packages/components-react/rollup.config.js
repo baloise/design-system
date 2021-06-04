caa89f6300de4a39ec0c4d2ec274488386de57e2
hirsch <gery.hirschfeld@w3tec.ch>
2021-06-04 08:10:59 +0200

    chore(react): add build process

--- packages/components-react/rollup.config.js
+++ packages/components-react/rollup.config.js
@@ -1,25 +1,47 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
 export default {
-  input: 'dist-transpiled/index.js',
+  // input: 'dist-transpiled/index.js',
+  input: 'src/index.ts',
   output: [
+    // {
+    //   dir: 'dist/',
+    //   entryFileNames: '[name].esm.js',
+    //   chunkFileNames: '[name]-[hash].esm.js',
+    //   format: 'es',
+    //   sourcemap: true,
+    // },
+    // {
+    //   dir: 'dist/',
+    //   format: 'commonjs',
+    //   preferConst: true,
+    //   sourcemap: true,
+    // },
     {
       dir: 'dist/',
       entryFileNames: '[name].esm.js',
       chunkFileNames: '[name]-[hash].esm.js',
-      format: 'es',
+      format: 'esm',
+      preserveModules: true,
       sourcemap: true,
     },
-    {
-      dir: 'dist/',
-      format: 'commonjs',
-      preferConst: true,
-      sourcemap: true,
-    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+    }),
   ],
   external: [
     '@baloise/design-system-components',
     '@baloise/design-system-components/loader',
     '@baloise/design-system-components/dist/custom-elements',
-    'vue',
-    'lodash',
+    'react',
+    'react-dom',
   ],
 }
