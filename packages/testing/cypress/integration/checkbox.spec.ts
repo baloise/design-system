cb94f8654362cf9342674888c44f53459c6a0a96
hirsch <gery.hirschfeld@w3tec.ch>
2021-09-13 15:52:13 +0200

    chore(): enable test

--- packages/testing/cypress/integration/checkbox.spec.ts
+++ packages/testing/cypress/integration/checkbox.spec.ts
@@ -1,32 +1,32 @@
 import { app } from '../support/app'
 
-describe.only('Checkbox', () => {
+describe('Checkbox', () => {
   let page = app.getCheckboxPage()
 
-  // it('should contain label', () => {
-  //   page.open()
-  //   cy.get(page.normalCheckboxElement).contains('Label')
-  // })
-
-  it.only('should be checkable', () => {
+  it('should contain label', () => {
     page.open()
-    cy.get(page.normalCheckboxElement).should('not.be.checked').and('not.be.disabled')
-    // cy.get(page.normalCheckboxElement).check()
-    // cy.get(page.normalCheckboxElement).should('be.checked')
-    // cy.get(page.normalCheckboxElement).check()
-    // cy.get(page.normalCheckboxElement).should('not.be.checked')
+    cy.get(page.normalCheckboxElement).contains('Label')
   })
 
-  // it('should be disableable', () => {
-  //   page.open()
-  //   cy.get(page.normalCheckboxElement).should('not.be.disabled')
-  //   cy.get(page.disabledCheckboxElement).should('be.disabled')
-  // })
+  it('should be checkable', () => {
+    page.open()
+    cy.get(page.normalCheckboxElement).should('not.be.checked').and('not.be.disabled')
+    cy.get(page.normalCheckboxElement).check()
+    cy.get(page.normalCheckboxElement).should('be.checked')
+    cy.get(page.normalCheckboxElement).check()
+    cy.get(page.normalCheckboxElement).should('not.be.checked')
+  })
 
-  // it('should be focusable', () => {
-  //   page.open()
-  //   cy.get(page.normalCheckboxElement).should('not.be.focused')
-  //   cy.get(page.normalCheckboxElement).focus()
-  //   cy.get(page.normalCheckboxElement).should('be.focused')
-  // })
+  it('should be disableable', () => {
+    page.open()
+    cy.get(page.normalCheckboxElement).should('not.be.disabled')
+    cy.get(page.disabledCheckboxElement).should('be.disabled')
+  })
+
+  it('should be focusable', () => {
+    page.open()
+    cy.get(page.normalCheckboxElement).should('not.be.focused')
+    cy.get(page.normalCheckboxElement).focus()
+    cy.get(page.normalCheckboxElement).should('be.focused')
+  })
 })
