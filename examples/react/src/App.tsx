fbd0c556affec8fa0945ad69ef69f113c864f0d7
hirsch <gery.hirschfeld@w3tec.ch>
2021-09-20 11:15:59 +0200

    chore(react): add basic example

---
+++ examples/react/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from './logo.svg'
+import {} from '@baloise/design-system-components-react'
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
