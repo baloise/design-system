0b0dfb742f9eec7ff209b84bee8fcd558bf18ab8
hirsch <gery.hirschfeld@w3tec.ch>
2021-09-29 15:37:58 +0200

    chore(): update

--- examples/angular-shared-libs/projects/angular-shared-lib/src/lib/angular-shared-lib.component.ts
+++ examples/angular-shared-libs/projects/angular-shared-lib/src/lib/angular-shared-lib.component.ts
@@ -4,10 +4,19 @@ import { BalValidators } from '@baloise/design-system-components-angular'
 
 @Component({
   selector: 'lib-angular-shared-lib',
-  template: `<form [formGroup]="form" (ngSubmit)="onSubmit()">
-    <bal-datepicker formControlName="birthdate" placeholder="Placeholder"></bal-datepicker>
-    <bal-ng-error controlName="birthdate" error="isRequired">This field is required</bal-ng-error>
-  </form> `,
+  template: `<bal-card>
+    <bal-card-content [formGroup]="form">
+      <bal-field expanded>
+        <bal-field-label required>Label</bal-field-label>
+        <bal-field-control>
+          <bal-datepicker expanded formControlName="birthdate"></bal-datepicker>
+        </bal-field-control>
+        <bal-field-message color="danger">
+          <bal-ng-error controlName="birthdate" error="isRequired">This field is required</bal-ng-error>
+        </bal-field-message>
+      </bal-field>
+    </bal-card-content>
+  </bal-card> `,
   styles: [],
 })
 export class AngularSharedLibComponent implements OnInit {
@@ -18,8 +27,4 @@ export class AngularSharedLibComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-
-  onSubmit() {
-    alert('Form is submitted!')
-  }
 }
