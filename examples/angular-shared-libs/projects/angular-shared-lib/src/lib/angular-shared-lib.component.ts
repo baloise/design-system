bb4291c304707e851ed17b8551be27286e691801
hirsch <gery.hirschfeld@w3tec.ch>
2021-09-29 14:03:57 +0200

    chore(): small changes

--- examples/angular-shared-libs/projects/angular-shared-lib/src/lib/angular-shared-lib.component.ts
+++ examples/angular-shared-libs/projects/angular-shared-lib/src/lib/angular-shared-lib.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'lib-angular-shared-lib',
-  template: ` <p>angular-shared-lib works! Still with Petar</p> `,
+  template: ` <p>angular-shared-lib works! Still with Petar and Gery 2</p> `,
   styles: [],
 })
 export class AngularSharedLibComponent implements OnInit {
