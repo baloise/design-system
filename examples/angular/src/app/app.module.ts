3edfc77d933d6acbb6e7b801c47d3fc220a85a22
hirsch <gery.hirschfeld@w3tec.ch>
2021-10-07 10:36:30 +0200

    docs(): add shared lib example for angular

--- examples/angular/src/app/app.module.ts
+++ examples/angular/src/app/app.module.ts
@@ -2,28 +2,30 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 import { BaloiseDesignSystemModule } from '@baloise/design-system-components-angular'
-// import { BaloiseAngularPipesModule } from '@baloise/web-app-ng-utils'
+import { BaloiseAngularPipesModule } from '@baloise/web-app-ng-utils'
 import { AngularSharedLibModule } from '@baloise/example-angular-shared-lib'
-// import { AgGridModule } from 'ag-grid-angular'
+import { AgGridModule } from 'ag-grid-angular'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-// import { FormPageComponent } from './pages/form/form-page.component'
-// import { TablePageComponent } from './pages/table/table-page.component'
-// import { ModalPageComponent } from './pages/modal/modal-page.component'
-// import { PipesPageComponent } from './pages/pipes/pipes-page.component'
-// import { ServicesPageComponent } from './pages/services/services-page.component'
-// import { ModalComponent } from './pages/modal/modal.component'
+import { FormPageComponent } from './pages/form/form-page.component'
+import { TablePageComponent } from './pages/table/table-page.component'
+import { ModalPageComponent } from './pages/modal/modal-page.component'
+import { PipesPageComponent } from './pages/pipes/pipes-page.component'
+import { ServicesPageComponent } from './pages/services/services-page.component'
+import { ModalComponent } from './pages/modal/modal.component'
+import { SharedPageComponent } from './pages/shared/shared-page.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    // FormPageComponent,
-    // TablePageComponent,
-    // ModalPageComponent,
-    // ModalComponent,
-    // PipesPageComponent,
-    // ServicesPageComponent,
+    FormPageComponent,
+    TablePageComponent,
+    ModalPageComponent,
+    ModalComponent,
+    PipesPageComponent,
+    ServicesPageComponent,
+    SharedPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,8 +34,8 @@ import { AppComponent } from './app.component'
     ReactiveFormsModule,
     AngularSharedLibModule,
     BaloiseDesignSystemModule.forRoot(),
-    // AgGridModule.withComponents([]),
-    // BaloiseAngularPipesModule,
+    AgGridModule.withComponents([]),
+    BaloiseAngularPipesModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
