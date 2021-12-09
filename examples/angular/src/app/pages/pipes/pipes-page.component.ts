6b61a934b893a1407ab13169ea0bd9d040c3df2d
hirsch <gery.hirschfeld@w3tec.ch>
2021-12-09 09:24:20 +0100

    chore(): transfer example to seperate repo

--- examples/angular/src/app/pages/pipes/pipes-page.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-pipes',
-  templateUrl: './pipes-page.component.html',
-})
-export class PipesPageComponent {}
--- examples/angular/src/app/pages/shared/shared-page.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-shared',
-  templateUrl: './shared-page.component.html',
-})
-export class SharedPageComponent {}
