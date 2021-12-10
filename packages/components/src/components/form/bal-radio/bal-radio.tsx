72f3a880ee89cb2884d40c97cefa893544243f38
hirsch <gery.hirschfeld@w3tec.ch>
2021-12-10 11:34:24 +0100

    fix(radio): adjust space between radio and label

--- packages/components/src/components/form/bal-radio/bal-radio.tsx
+++ packages/components/src/components/form/bal-radio/bal-radio.tsx
@@ -163,7 +163,7 @@ export class Radio implements ComponentInterface {
         >
           <bal-text
             class={{
-              'pl-6': !this.isEmpty && this.interface !== 'select-button',
+              'has-padding-left': !this.isEmpty && this.interface !== 'select-button',
             }}
           >
             <slot></slot>
