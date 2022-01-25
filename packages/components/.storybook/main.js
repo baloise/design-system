5b7883eabb6203b770ac97b98016197ea7f0609f
hirsch <gerhard.hirschfeld@baloise.ch>
2022-01-25 22:56:25 +0100

    chore: remove unnecessary debug mode
    Use npm run dev instead

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,5 +1,3 @@
-const isDebugMode = process.env.STORYBOOK_MODE === 'debug'
-
 module.exports = {
   stories: ['../src/**/intro.stories.mdx', '../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', './addons/source/index.js'],
@@ -10,9 +8,5 @@ module.exports = {
   previewHead: head => `
     ${head}
     <link rel="stylesheet" type="text/css" href="assets/css/design-system-table.css" />
-    <link rel="stylesheet" type="text/css" href="assets/css/theme.css" />
-    ${!isDebugMode ? '<link rel="stylesheet" type="text/css" href="build/design-system-components.css" />' : ''}
-    ${isDebugMode ? '<script type="module" src="build/design-system-components.esm.js"></script>' : ''}
-    ${isDebugMode ? '<script nomodule src="build/design-system-components.js"></script>' : ''}
-    `,
+    <link rel="stylesheet" type="text/css" href="assets/css/theme.css" />`,
 }
