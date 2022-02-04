63c7cc0b9b31305d7419318e7cbe36283911bf0c
hirsch <gerhard.hirschfeld@baloise.ch>
2022-02-04 08:14:07 +0100

    chore: add app as component

--- packages/components-vue/src/plugin.ts
+++ packages/components-vue/src/plugin.ts
@@ -2,6 +2,7 @@ import { Plugin } from 'vue'
 import { BalConfig, initialize } from '@baloise/design-system-components'
 import { applyDirectives } from './directives'
 import { applyComponents } from './components.generated'
+import { BalApp } from './components/BalApp'
 
 interface BaloiseDesignSystemVueConfig {
   defineCustomeElementTag?: boolean
@@ -17,6 +18,8 @@ export const BaloiseDesignSystem: Plugin = {
     }
 
     applyDirectives(app)
+
+    app.component('bal-app', BalApp)
     applyComponents(app)
   },
 }
