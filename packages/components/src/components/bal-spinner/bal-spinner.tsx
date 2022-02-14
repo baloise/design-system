e5a164f9499a11f16362cf1a0d4b0b0eabfd0bca
hirsch <gerhard.hirschfeld@baloise.ch>
2022-02-14 16:16:03 +0100

    chore: update spinner

--- packages/components/src/components/bal-spinner/bal-spinner.tsx
+++ packages/components/src/components/bal-spinner/bal-spinner.tsx
@@ -1,6 +1,6 @@
 import { Component, h, Host, Prop, Element } from '@stencil/core'
 import Lottie from 'lottie-web/build/player/lottie_light_html'
-import type { AnimationItem, AnimationDirection } from 'lottie-web/build/player/lottie_light_html'
+import type { AnimationItem } from 'lottie-web/build/player/lottie_light_html'
 import { SpinnerAnimationData } from './bal-spinner.animation'
 
 @Component({
@@ -48,17 +48,17 @@ export class Spinner {
     this.animation = Lottie.loadAnimation({
       container: this.el,
       renderer: 'svg',
-      loop: false,
+      loop: true,
       autoplay: true,
       animationData: SpinnerAnimationData(this.getColor()),
     })
-    let direction: AnimationDirection = 1
-    this.animation.setSpeed(0.75)
-    this.animation.addEventListener('complete', () => {
-      direction = (direction * -1) as AnimationDirection
-      this.animation.setDirection(direction)
-      this.animation.play()
-    })
+    // let direction: AnimationDirection = 1
+    // this.animation.setSpeed(0.75)
+    // this.animation.addEventListener('complete', () => {
+    //   direction = (direction * -1) as AnimationDirection
+    //   this.animation.setDirection(direction)
+    //   this.animation.play()
+    // })
   }
 
   disconnectedCallback() {
