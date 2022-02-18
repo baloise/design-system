7d2f7e549a1dcff250ef57c214c25ca83e58c634
hirsch <gerhard.hirschfeld@baloise.ch>
2022-02-18 15:50:27 +0100

    fix(react): change .mjs to .esm.js

--- packages/components-react/rollup.config.js
+++ packages/components-react/rollup.config.js
@@ -2,11 +2,12 @@ import baseConfig from '../../rollup.base'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import sourcemaps from 'rollup-plugin-sourcemaps'
 
 export default {
   ...baseConfig,
   external: id => {
     return id.startsWith('react') || id.startsWith('@baloise')
   },
-  plugins: [nodeResolve(), commonjs(), peerDepsExternal()],
+  plugins: [nodeResolve(), commonjs(), peerDepsExternal(), sourcemaps()],
 }
