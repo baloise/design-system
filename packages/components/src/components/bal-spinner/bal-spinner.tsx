b2135b8f10b2fc3c1d76a121874b73f6675328d6
hirsch <gerhard.hirschfeld@baloise.ch>
2022-02-23 22:32:32 +0100

    fix(spinner): change colors

--- packages/components/src/components/bal-spinner/bal-spinner.tsx
+++ packages/components/src/components/bal-spinner/bal-spinner.tsx
@@ -2,6 +2,7 @@ import { Component, h, Host, Prop, Element } from '@stencil/core'
 import Lottie from 'lottie-web/build/player/lottie_light_html'
 import type { AnimationItem } from 'lottie-web/build/player/lottie_light_html'
 import { SpinnerAnimationData } from './bal-spinner.animation'
+import { flatten } from 'lottie-colorify'
 
 @Component({
   tag: 'bal-spinner',
@@ -50,15 +51,8 @@ export class Spinner {
       renderer: 'svg',
       loop: true,
       autoplay: true,
-      animationData: SpinnerAnimationData(this.getColor()),
+      animationData: flatten(this.getColor(), SpinnerAnimationData(this.getColor())),
     })
-    // let direction: AnimationDirection = 1
-    // this.animation.setSpeed(0.75)
-    // this.animation.addEventListener('complete', () => {
-    //   direction = (direction * -1) as AnimationDirection
-    //   this.animation.setDirection(direction)
-    //   this.animation.play()
-    // })
   }
 
   disconnectedCallback() {
