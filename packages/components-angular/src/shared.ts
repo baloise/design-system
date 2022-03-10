0d377c7a14f558cfaeb11622a30e209d2ff683b3
hirsch <gerhard.hirschfeld@baloise.ch>
2022-03-10 08:18:20 +0100

    chore: add forms to shared

--- packages/components-angular/src/shared.ts
+++ packages/components-angular/src/shared.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
 import { BooleanValueAccessor } from './directives/boolean-value-accessor'
 import { TextValueAccessor } from './directives/text-value-accessor'
@@ -24,6 +25,6 @@ const DECLARATIONS = [
 @NgModule({
   declarations: [DECLARATIONS],
   exports: [DECLARATIONS],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class BalSharedModule {}
