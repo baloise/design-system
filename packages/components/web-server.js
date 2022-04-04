ea6d193b8e9a967cb1359e5e6f1110fb00c8a015
hirsch <gerhard.hirschfeld@baloise.ch>
2022-04-04 14:03:34 +0200

    chore: add base image flow 6

---
+++ packages/components/web-server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const serveIndex = require('serve-index')
+
+const app = express()
+
+app.set('port', 3333)
+
+app.use(express.static(path.join(__dirname, 'www')))
+app.use(serveIndex(path.join(__dirname, 'www')))
+
+app.listen(app.get('port'), () => {
+  console.log('The server is running on http://localhost:' + app.get('port'))
+})
