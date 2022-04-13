a213e295ff0cc9a8c712239d65ef7910b839a296
hirsch <gerhard.hirschfeld@baloise.ch>
2022-04-13 16:24:26 +0200

    feat(breakpoint): add isPlatform util

--- packages/components/src/global.ts
+++ packages/components/src/global.ts
@@ -1,13 +1,17 @@
 import { balToastController } from './components/notice/bal-toast/bal-toast.controller'
 import { balSnackbarController } from './components/notice/bal-snackbar/bal-snackbar.controller'
 import { initialize } from './config'
+import { setupPlatforms } from './utils/platform'
 
 export default function () {
   if (typeof (window as any) !== 'undefined') {
     const win = window as any
-    const BaloiseDesignSystem = (win.BaloiseDesignSystem = win.BaloiseDesignSystem || {})
-    initialize()
-    BaloiseDesignSystem.toastController = balToastController
-    BaloiseDesignSystem.snackbarController = balSnackbarController
+    win.BaloiseDesignSystem = win.BaloiseDesignSystem || {}
+
+    initialize({}, win)
+    setupPlatforms(win)
+
+    win.BaloiseDesignSystem.toastController = balToastController
+    win.BaloiseDesignSystem.snackbarController = balSnackbarController
   }
 }
