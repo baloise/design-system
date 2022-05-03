819337e57b5f71dca487e32074223eecf959206e
hirsch <gerhard.hirschfeld@baloise.ch>
2022-05-03 14:30:58 +0200

    chore: format files

--- packages/components/src/components/bal-list/stories/bal-list.stories.ts
+++ packages/components/src/components/bal-list/stories/bal-list.stories.ts
@@ -218,7 +218,7 @@ export const AccordionList = args => ({
       </bal-list-item-content>
     </bal-list-item-accordion-head>
     <bal-list-item-accordion-body>
-      <p>
+      <p class="pb-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </bal-list-item-accordion-body>
@@ -234,7 +234,7 @@ export const AccordionList = args => ({
     </bal-list-item-content>
   </bal-list-item-accordion-head>
   <bal-list-item-accordion-body>
-    <p>
+    <p class="pb-4">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
   </bal-list-item-accordion-body>
