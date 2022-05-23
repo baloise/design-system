5f0a1ab17472ae1d5bb6496e5baaac2100362c6f
Mladen Planinicic <mladenplaninicic@gmail.com>
2022-05-23 11:31:26 +0200

    feat(bal-hint): use variables in sass

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -1616,6 +1616,12 @@ export namespace Components {
          */
         "size": Props.BalStageSize;
     }
+    interface BalStageBackLink {
+        /**
+          * Specifies the URL of the page the link goes to
+         */
+        "href": string;
+    }
     interface BalStageBody {
         /**
           * If `true` the inner container uses the compact layout.
@@ -2402,6 +2408,12 @@ declare global {
         prototype: HTMLBalStageElement;
         new (): HTMLBalStageElement;
     };
+    interface HTMLBalStageBackLinkElement extends Components.BalStageBackLink, HTMLStencilElement {
+    }
+    var HTMLBalStageBackLinkElement: {
+        prototype: HTMLBalStageBackLinkElement;
+        new (): HTMLBalStageBackLinkElement;
+    };
     interface HTMLBalStageBodyElement extends Components.BalStageBody, HTMLStencilElement {
     }
     var HTMLBalStageBodyElement: {
@@ -2563,6 +2575,7 @@ declare global {
         "bal-snackbar": HTMLBalSnackbarElement;
         "bal-spinner": HTMLBalSpinnerElement;
         "bal-stage": HTMLBalStageElement;
+        "bal-stage-back-link": HTMLBalStageBackLinkElement;
         "bal-stage-body": HTMLBalStageBodyElement;
         "bal-stage-foot": HTMLBalStageFootElement;
         "bal-stage-head": HTMLBalStageHeadElement;
@@ -4259,6 +4272,12 @@ declare namespace LocalJSX {
          */
         "size"?: Props.BalStageSize;
     }
+    interface BalStageBackLink {
+        /**
+          * Specifies the URL of the page the link goes to
+         */
+        "href": string;
+    }
     interface BalStageBody {
         /**
           * If `true` the inner container uses the compact layout.
@@ -4654,6 +4673,7 @@ declare namespace LocalJSX {
         "bal-snackbar": BalSnackbar;
         "bal-spinner": BalSpinner;
         "bal-stage": BalStage;
+        "bal-stage-back-link": BalStageBackLink;
         "bal-stage-body": BalStageBody;
         "bal-stage-foot": BalStageFoot;
         "bal-stage-head": BalStageHead;
@@ -4755,6 +4775,7 @@ declare module "@stencil/core" {
             "bal-snackbar": LocalJSX.BalSnackbar & JSXBase.HTMLAttributes<HTMLBalSnackbarElement>;
             "bal-spinner": LocalJSX.BalSpinner & JSXBase.HTMLAttributes<HTMLBalSpinnerElement>;
             "bal-stage": LocalJSX.BalStage & JSXBase.HTMLAttributes<HTMLBalStageElement>;
+            "bal-stage-back-link": LocalJSX.BalStageBackLink & JSXBase.HTMLAttributes<HTMLBalStageBackLinkElement>;
             "bal-stage-body": LocalJSX.BalStageBody & JSXBase.HTMLAttributes<HTMLBalStageBodyElement>;
             "bal-stage-foot": LocalJSX.BalStageFoot & JSXBase.HTMLAttributes<HTMLBalStageFootElement>;
             "bal-stage-head": LocalJSX.BalStageHead & JSXBase.HTMLAttributes<HTMLBalStageHeadElement>;
