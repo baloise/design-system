69278b013b305a23ccce2f36fcb6b8ef3f6c6d81
Mladen Planinicic <mladen.planinicic@baloise.ch>
2022-06-06 14:30:15 +0200

    chore(bal-input-stepper): change scss to sass and add bem

--- packages/components/src/components/form/bal-input-stepper/bal-input-stepper.tsx
+++ packages/components/src/components/form/bal-input-stepper/bal-input-stepper.tsx
@@ -25,6 +25,7 @@ import {
   defaultConfig,
   detachComponentToConfig,
 } from '../../../config'
+import { BEM } from '../../../utils/bem'
 
 @Component({
   tag: 'bal-input-stepper',
@@ -158,15 +159,29 @@ export class InputStepper implements ComponentInterface, BalConfigObserver, Form
       label.htmlFor = this.inputId
     }
 
+    const block = BEM.block('input-stepper')
+    const elInput = block.element('input')
+    const elWrapper = block.element('wrapper')
+    const elText = elWrapper.element('text')
+
     return (
       <Host
         aria-disabled={this.disabled ? 'true' : null}
         class={{
           'is-disabled': this.disabled || this.readonly,
           'is-invalid': this.invalid,
+          ...block.class(),
         }}
       >
-        <div class="is-flex fg-1 is-justify-content-center is-align-items-center">
+        <div
+          class={{
+            'is-flex': true,
+            'fg-1': true,
+            'is-justify-content-center': true,
+            'is-align-items-center': true,
+            ...elWrapper.class(),
+          }}
+        >
           <bal-button
             size="small"
             square
@@ -176,7 +191,14 @@ export class InputStepper implements ComponentInterface, BalConfigObserver, Form
             disabled={this.disabled || this.readonly || this.value <= this.min}
             onClick={_ => this.decrease()}
           ></bal-button>
-          <bal-text space="none" color={this.disabled || this.readonly ? 'grey' : this.invalid ? 'danger' : ''} bold>
+          <bal-text
+            space="none"
+            color={this.disabled || this.readonly ? 'grey' : this.invalid ? 'danger' : ''}
+            bold
+            class={{
+              ...elText.class(),
+            }}
+          >
             {formatLocaleNumber(`${this.language}-${this.region}`, this.value)}
           </bal-text>
           <bal-button
@@ -190,6 +212,9 @@ export class InputStepper implements ComponentInterface, BalConfigObserver, Form
           ></bal-button>
         </div>
         <input
+          class={{
+            ...elInput.class(),
+          }}
           type="text"
           value={this.value}
           name={this.name}
