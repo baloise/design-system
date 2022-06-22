5c8e8c6e93d04abdac121f24b3cdca36d3dec403
hirsch <gerhard.hirschfeld@baloise.ch>
2022-06-22 13:58:32 +0200

    fix(accordion): remove double line for links and buttons. Closes: #127

--- packages/components/src/components/bal-list/stories/bal-list.stories.ts
+++ packages/components/src/components/bal-list/stories/bal-list.stories.ts
@@ -234,6 +234,8 @@ export const AccordionList = args => ({
       <p class="pb-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
+      <a class="is-link">Link</a>
+      <button class="button">Button</button>
     </bal-list-item-accordion-body>
   </bal-list-item>
   <bal-list-item accordion>
