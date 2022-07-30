e0f5f9d133759fc974884dc7b26bff0a1410b320
hirsch <gerhard.hirschfeld@baloise.ch>
2022-07-30 10:31:46 +0200

    chore: add react output target

--- .build/copy.js
+++ .build/copy.js
@@ -13,8 +13,9 @@ async function main() {
   await copyReadme('packages/icons')
   await copyReadme('packages/fonts')
   await copyReadme('packages/testing')
-  await copyReadme('packages/output-target-angular')
-  await copyReadme('packages/output-target-vue')
+  await copyReadme('packages/output-targets/angular')
+  await copyReadme('packages/output-targets/vue')
+  await copyReadme('packages/output-targets/react')
   log.success('Resources are copied')
 }
 
