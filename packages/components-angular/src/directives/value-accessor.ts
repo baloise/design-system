039596d9544dc25daadbb31d38570f9e08b5f721
hirsch <gerhard.hirschfeld@baloise.ch>
2022-07-30 10:45:11 +0200

    chore: add react output target

--- packages/components-angular/src/directives/value-accessor.ts
+++ packages/components-angular/src/directives/value-accessor.ts
@@ -1,44 +1,39 @@
-import { Directive, ElementRef, HostListener } from '@angular/core'
-import { ControlValueAccessor } from '@angular/forms'
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Directive()
 export class ValueAccessor implements ControlValueAccessor {
-  private onChange: (value: any) => void = () => {
-    /**/
-  }
-  private onTouched: () => void = () => {
-    /**/
-  }
-  protected lastValue: any
+
+  private onChange: (value: any) => void = () => {/**/};
+  private onTouched: () => void = () => {/**/};
+  protected lastValue: any;
 
   constructor(protected el: ElementRef) {}
 
   writeValue(value: any) {
-    this.el.nativeElement.value = this.lastValue = value == null ? '' : value
+    this.el.nativeElement.value = this.lastValue = value == null ? '' : value;
   }
 
   handleChangeEvent(value: any) {
     if (value !== this.lastValue) {
-      this.lastValue = value
-      this.onChange(value)
+      this.lastValue = value;
+      this.onChange(value);
     }
   }
 
-  @HostListener('balBlur', ['$event.target'])
-  handleBlurEvent(el: any) {
-    if (el === this.el.nativeElement) {
-      this.onTouched()
-    }
+  @HostListener('focusout')
+  _handleBlurEvent() {
+    this.onTouched();
   }
 
   registerOnChange(fn: (value: any) => void) {
-    this.onChange = fn
+    this.onChange = fn;
   }
   registerOnTouched(fn: () => void) {
-    this.onTouched = fn
+    this.onTouched = fn;
   }
 
   setDisabledState(isDisabled: boolean) {
-    this.el.nativeElement.disabled = isDisabled
+    this.el.nativeElement.disabled = isDisabled;
   }
 }
