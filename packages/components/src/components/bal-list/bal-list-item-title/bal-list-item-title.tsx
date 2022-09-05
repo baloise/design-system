d72324d0a899553a5ab48bc8121651d5e504928a
mladenplaninicic <mladenplaninicic@gmail.com>
2022-09-05 12:46:39 +0200

    chore(list): change default list item title level to h5 and set h4 in navigation

--- packages/components/src/components/bal-list/bal-list-item-title/bal-list-item-title.tsx
+++ packages/components/src/components/bal-list/bal-list-item-title/bal-list-item-title.tsx
@@ -10,7 +10,7 @@ export class ListItemTitle {
   /**
    * Heading level with value 'h4' on default
    */
-  @Prop() level: Props.BalHeadingLevel = 'h4'
+  @Prop() level: Props.BalHeadingLevel = 'h5'
 
   render() {
     return (
