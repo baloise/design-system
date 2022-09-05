730ffa282462ff7d2ce9fba04cb9e4e6ad399e49
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-09-05 09:12:10 +0200

    chore: clean up left overs

--- packages/components-react/rollup.config.js
+++ packages/components-react/rollup.config.js
@@ -1,16 +1,3 @@
-// import baseConfig from '../../rollup.base'
-// import { nodeResolve } from '@rollup/plugin-node-resolve'
-// import commonjs from '@rollup/plugin-commonjs'
-// import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-
-// export default {
-//   ...baseConfig,
-//   external: id => {
-//     return id.startsWith('react') || id.startsWith('@baloise')
-//   },
-//   plugins: [nodeResolve(), commonjs(), peerDepsExternal()],
-// }
-
 import config from '../../rollup.base'
 
 export default Object.assign(config())
--- packages/components-vue/rollup.config.js
+++ packages/components-vue/rollup.config.js
@@ -1,12 +1,3 @@
-// import baseConfig from '../../rollup.base'
-
-// export default {
-//   ...baseConfig,
-//   external: id => {
-//     return id.startsWith('vue') || id.startsWith('@baloise')
-//   },
-// }
-
 import config from '../../rollup.base'
 
 export default Object.assign(config())
