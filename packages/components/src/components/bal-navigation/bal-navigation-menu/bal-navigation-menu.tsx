a6c0dab10925818b646e6b13ea1e221f35fc07d5
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-09-14 08:49:51 +0200

    fix(navigation): adjust spacing menu content

--- packages/components/src/components/bal-navigation/bal-navigation-menu/bal-navigation-menu.tsx
+++ packages/components/src/components/bal-navigation/bal-navigation-menu/bal-navigation-menu.tsx
@@ -32,59 +32,61 @@ export class NavigationMenu {
           </div>
         )}
         {this.elements && (
-          <div
-            class={{
-              'columns is-multiline': true,
-              ...navMenuEl.element('wrapper').class(),
-            }}
-          >
-            {this.elements.some(subLevel => subLevel.color === 'white') && (
-              <div
-                class={{
-                  'bal-nav__menu__white-list__wrapper column is-full is-6-desktop is-two-thirds-widescreen': true,
-                }}
-              >
-                <div class={{ ...navMenuEl.element('white-list').class() }}>
-                  {this.elements
-                    .filter(subLevel => subLevel.color === 'white')
-                    .map(block => {
-                      return (
-                        block && (
-                          <bal-navigation-menu-list headline={block.label} href={block.link} target={block.target}>
-                            <div slot="links">
-                              {block.subLevels?.map(item => (
-                                <bal-navigation-menu-list-item href={item.link} target={item.target}>
-                                  {item.label}
-                                </bal-navigation-menu-list-item>
-                              ))}
-                            </div>
-                          </bal-navigation-menu-list>
+          <div style={{ overflow: 'hidden' }}>
+            <div
+              class={{
+                'columns is-multiline': true,
+                ...navMenuEl.element('wrapper').class(),
+              }}
+            >
+              {this.elements.some(subLevel => subLevel.color === 'white') && (
+                <div
+                  class={{
+                    'bal-nav__menu__white-list__wrapper column is-full is-6-desktop is-two-thirds-widescreen': true,
+                  }}
+                >
+                  <div class={{ ...navMenuEl.element('white-list').class() }}>
+                    {this.elements
+                      .filter(subLevel => subLevel.color === 'white')
+                      .map(block => {
+                        return (
+                          block && (
+                            <bal-navigation-menu-list headline={block.label} href={block.link} target={block.target}>
+                              <div slot="links">
+                                {block.subLevels?.map(item => (
+                                  <bal-navigation-menu-list-item href={item.link} target={item.target}>
+                                    {item.label}
+                                  </bal-navigation-menu-list-item>
+                                ))}
+                              </div>
+                            </bal-navigation-menu-list>
+                          )
                         )
-                      )
-                    })}
+                      })}
+                  </div>
                 </div>
-              </div>
-            )}
-            {this.elements.some(subLevel => subLevel.color === 'grey') && (
-              <div
-                class={{
-                  'column is-full is-6-desktop is-one-third-widescreen': true,
-                  ...navMenuEl.element('grey-list').class(),
-                }}
-              >
-                {this.elements
-                  .filter(subLevel => subLevel.color === 'grey')
-                  .map(block => (
-                    <bal-navigation-menu-list headline={block.label} href={block.link} color={block.color}>
-                      <div slot="links">
-                        {block.subLevels?.map(item => (
-                          <bal-navigation-menu-list-item href={item.link}>{item.label}</bal-navigation-menu-list-item>
-                        ))}
-                      </div>
-                    </bal-navigation-menu-list>
-                  ))}
-              </div>
-            )}
+              )}
+              {this.elements.some(subLevel => subLevel.color === 'grey') && (
+                <div
+                  class={{
+                    'column is-full is-6-desktop is-one-third-widescreen': true,
+                    ...navMenuEl.element('grey-list').class(),
+                  }}
+                >
+                  {this.elements
+                    .filter(subLevel => subLevel.color === 'grey')
+                    .map(block => (
+                      <bal-navigation-menu-list headline={block.label} href={block.link} color={block.color}>
+                        <div slot="links">
+                          {block.subLevels?.map(item => (
+                            <bal-navigation-menu-list-item href={item.link}>{item.label}</bal-navigation-menu-list-item>
+                          ))}
+                        </div>
+                      </bal-navigation-menu-list>
+                    ))}
+                </div>
+              )}
+            </div>
           </div>
         )}
       </Host>
