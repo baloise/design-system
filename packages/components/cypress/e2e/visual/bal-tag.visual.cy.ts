4d9fe747736a6e6c7fd674d72ac44d8564d4ec60
mladenplaninicic <mladenplaninicic@gmail.com>
2022-09-16 11:27:48 +0200

    chore(tag): refactor visual test

--- packages/components/cypress/e2e/visual/bal-tag.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-tag.visual.cy.ts
@@ -1,10 +1,8 @@
 import { byTestId } from '../../../../testing/src'
 
-describe('Visual Regression Tests', () => {
-  describe('Tag', () => {
-    before(() => cy.page('/components/bal-tag/test/bal-tag.visual.html'))
+describe('Tag', () => {
+  before(() => cy.page('/components/bal-tag/test/bal-tag.visual.html'))
 
-    it('basic component', () => cy.get(byTestId('basic')).compareSnapshot('tag-basic', 0.0))
-    it('component variants', () => cy.compareSnapshot('tag-variants', 0.0))
-  })
+  it('basic component', () => cy.get(byTestId('basic')).compareSnapshot('tag-basic', 0.0))
+  it('component variants', () => cy.compareSnapshot('tag-variants', 0.0))
 })
