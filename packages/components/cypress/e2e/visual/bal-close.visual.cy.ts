0516a83a18db19c293e06133052529f75dc25877
mladenplaninicic <mladenplaninicic@gmail.com>
2022-09-16 11:26:59 +0200

    chore(close): add visual test

---
+++ packages/components/cypress/e2e/visual/bal-close.visual.cy.ts
@@ -0,0 +1,8 @@
+import { byTestId } from '../../../../testing/src'
+
+describe('Close', () => {
+  before(() => cy.page('/components/bal-close/test/bal-close.visual.html'))
+
+  it('basic component', () => cy.get(byTestId('basic')).compareSnapshot('close-basic', 0.0))
+  it('component variants', () => cy.compareSnapshot('close-variants', 0.0))
+})
