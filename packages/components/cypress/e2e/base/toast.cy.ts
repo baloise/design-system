733ea60ca185cd16a41ccbd1a996aa309ad953dd
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-09-20 15:35:09 +0200

    test: update docs and tests

--- packages/components/cypress/e2e/base/toast.cy.ts
+++ packages/components/cypress/e2e/base/toast.cy.ts
@@ -1,12 +1,9 @@
-import { app } from '../../support/app'
-
-describe('Toast', () => {
-  const page = app.getToastPage()
+describe('bal-toast', () => {
+  before(() => cy.page('/components/notice/bal-toast/test/bal-toast.cy.html'))
 
   it('should navigate to Toast page and open Toast', () => {
-    page.open()
-    cy.get(page.toast).click()
-    cy.get(page.toastWarning).click()
+    cy.getByTestId('toast').click()
+    cy.getByTestId('toast-warning').click()
     cy.balToastFind().first().contains('Hi I am a default Toast! Hi I am a default Toast!')
     cy.balToastFind().eq(1).contains('Warning!')
   })
--- packages/components/cypress/e2e/visual/bal-close.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-close.visual.cy.ts
@@ -1,8 +1,11 @@
-import { byTestId } from '../../../../testing/src'
-
 describe('bal-close', () => {
   before(() => cy.page('/components/bal-close/test/bal-close.visual.html'))
 
-  it('basic component', () => cy.get(byTestId('basic')).compareSnapshot('close-basic', 0.0))
-  it('component variants', () => cy.compareSnapshot('close-variants', 0.0))
+  it('basic component', () => {
+    cy.getByTestId('basic').compareSnapshot('close-basic', 0.0)
+  })
+
+  it('component variants', () => {
+    cy.compareSnapshot('close-variants', 0.0)
+  })
 })
