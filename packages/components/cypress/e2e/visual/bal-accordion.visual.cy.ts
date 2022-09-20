93a3b1966bb2e83e2fb2c23a74ebf922b02acd1e
Joachim Prinzbach <joachim.prinzbach@baloise.ch>
2022-09-20 16:11:03 +0200

    test: add more accordion options

--- packages/components/cypress/e2e/visual/bal-accordion.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-accordion.visual.cy.ts
@@ -4,12 +4,33 @@ describe('bal-accordion', () => {
   it('basic component', () => {
     cy.platform('desktop')
     cy.getByTestId('basic').compareSnapshot('accordion-desktop', 0.0)
+    cy.getByTestId('basic').click()
+    cy.getByTestId('basic').compareSnapshot('accordion-desktop-open', 0.0)
+    cy.getByTestId('basic').click()
+    cy.getByTestId('basic').compareSnapshot('accordion-desktop-closed', 0.0)
 
     cy.platform('tablet')
-    cy.getByTestId('basic').compareSnapshot('accordion-desktop', 0.0)
+    cy.getByTestId('basic').compareSnapshot('accordion-tablet', 0.0)
+    cy.getByTestId('basic').click()
+    cy.getByTestId('basic').compareSnapshot('accordion-tablet-open', 0.0)
+    cy.getByTestId('basic').click()
+    cy.getByTestId('basic').compareSnapshot('accordion-tablet-closed', 0.0)
 
     cy.platform('mobile')
-    cy.getByTestId('basic').compareSnapshot('accordion-desktop', 0.0)
+    cy.getByTestId('basic').compareSnapshot('accordion-mobile', 0.0)
+    cy.getByTestId('basic').click()
+    cy.getByTestId('basic').compareSnapshot('accordion-mobile-open', 0.0)
+    cy.getByTestId('basic').click()
+    cy.getByTestId('basic').compareSnapshot('accordion-mobile-closed', 0.0)
+  })
+
+  it('with card', () => {
+    cy.platform('desktop')
+    cy.getByTestId('with-card').compareSnapshot('accordion-with-card', 0.0)
+    cy.getByTestId('with-card').click()
+    cy.getByTestId('with-card').compareSnapshot('accordion-with-card-open', 0.0)
+    cy.getByTestId('with-card').click()
+    cy.getByTestId('with-card').compareSnapshot('accordion-with-card-closed', 0.0)
   })
 
   it('component variants', () => {
