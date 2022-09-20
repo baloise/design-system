3698deae45d1a733ca4c651e43354ca16e231d66
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-09-20 13:32:40 +0200

    test(stage): create image of the whole page

--- packages/components/cypress/e2e/visual/bal-stage.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-stage.visual.cy.ts
@@ -1,29 +1,27 @@
-import { byTestId } from '../../../../testing/src'
-
 describe('bal-stage', () => {
   it('basic component', () => {
     cy.page('/components/bal-stage/test/bal-stage.visual.html')
 
     cy.platform('desktop')
-    cy.get(byTestId('basic')).compareSnapshot('stage-basic', 0.0)
+    cy.compareSnapshot('stage-basic', 0.0)
 
     cy.platform('tablet')
-    cy.get(byTestId('basic')).compareSnapshot('stage-basic-tablet', 0.0)
+    cy.compareSnapshot('stage-basic-tablet', 0.0)
 
     cy.platform('mobile')
-    cy.get(byTestId('basic')).compareSnapshot('stage-basic-mobile', 0.0)
+    cy.compareSnapshot('stage-basic-mobile', 0.0)
   })
 
   it('large stage', () => {
     cy.page('/components/bal-stage/test/bal-stage.large.visual.html')
 
     cy.platform('desktop')
-    cy.get(byTestId('large')).compareSnapshot('stage-large', 0.0)
+    cy.compareSnapshot('stage-large', 0.0)
 
     cy.platform('tablet')
-    cy.get(byTestId('large')).compareSnapshot('stage-large-tablet', 0.0)
+    cy.compareSnapshot('stage-large-tablet', 0.0)
 
     cy.platform('mobile')
-    cy.get(byTestId('large')).compareSnapshot('stage-large-mobile', 0.0)
+    cy.compareSnapshot('stage-large-mobile', 0.0)
   })
 })
