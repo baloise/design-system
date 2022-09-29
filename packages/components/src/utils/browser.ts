20175e1ed1d8c79bbd2df4ae767492f985e8015f
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-09-29 15:05:01 +0200

    fix(navigation): improve touch handling for scroll behaviour

--- packages/components/src/utils/browser.ts
+++ packages/components/src/utils/browser.ts
@@ -1,10 +1,15 @@
-type Browser = 'Safari' | 'others'
+type Browser = 'Safari' | 'touch' | 'others'
 
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+
+const isTouch = !!('ontouchstart' in window || (navigator as any).msMaxTouchPoints)
 
 export const isBrowser = (browser: Browser): boolean => {
   if (browser === 'Safari') {
     return isSafari
+  }
+  if (browser === 'touch') {
+    return isTouch
   }
   return false
 }
