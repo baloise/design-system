697ecc937add156602581131aa95e8f6befec826
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-14 16:25:19 +0200

    test(bal-data): add visual test

---
+++ packages/components/cypress/e2e/visual/bal-data.visual.cy.ts
@@ -0,0 +1,25 @@
+describe('bal-notification', () => {
+  before(() => cy.page('/components/bal-data/test/bal-data.visual.html'))
+
+  it('basic component', () => {
+    cy.platform('desktop')
+    cy.getByTestId('basic').compareSnapshot('notification-desktop', 0.0)
+
+    cy.platform('tablet')
+    cy.getByTestId('basic').compareSnapshot('notification-tablet', 0.0)
+
+    cy.platform('mobile')
+    cy.getByTestId('basic').compareSnapshot('notification-mobile', 0.0)
+  })
+
+  it('component variants', () => {
+    cy.platform('desktop')
+    cy.compareSnapshot('notification-variants-desktop', 0.0)
+
+    cy.platform('tablet')
+    cy.compareSnapshot('notification-variants-tablet', 0.0)
+
+    cy.platform('mobile')
+    cy.compareSnapshot('notification-variants-mobile', 0.0)
+  })
+})
