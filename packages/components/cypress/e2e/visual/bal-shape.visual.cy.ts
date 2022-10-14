1e0da79b3ee980dea15c780641569b8c523d51f3
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-14 13:50:45 +0200

    test(bal-shape): add platforms

--- packages/components/cypress/e2e/visual/bal-shape.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-shape.visual.cy.ts
@@ -1,7 +1,25 @@
 describe('bal-shape', () => {
   before(() => cy.page('/components/bal-shape/test/bal-shape.visual.html'))
 
-  it('basic component', () => cy.getByTestId('basic').compareSnapshot('shape-basic', 0.0))
+  it('basic component', () => {
+    cy.platform('desktop')
+    cy.getByTestId('basic').compareSnapshot('shape-desktop', 0.0)
 
-  it('component variants', () => cy.compareSnapshot('shape-variants', 0.0))
+    cy.platform('tablet')
+    cy.getByTestId('basic').compareSnapshot('shape-tablet', 0.0)
+
+    cy.platform('mobile')
+    cy.getByTestId('basic').compareSnapshot('shape-mobile', 0.0)
+  })
+
+  it('component variants', () => {
+    cy.platform('desktop')
+    cy.compareSnapshot('shape-variants-desktop', 0.0)
+
+    cy.platform('tablet')
+    cy.compareSnapshot('shape-variants-tablet', 0.0)
+
+    cy.platform('mobile')
+    cy.compareSnapshot('shape-variants-mobile', 0.0)
+  })
 })
