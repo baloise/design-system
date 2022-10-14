1e02992a6b97f19b816e1a1c543e48e9516f9219
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-14 12:37:37 +0200

    test(bal-shape): refactor

--- packages/components/cypress/e2e/visual/bal-shape.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-shape.visual.cy.ts
@@ -1,11 +1,7 @@
 describe('bal-shape', () => {
   before(() => cy.page('/components/bal-shape/test/bal-shape.visual.html'))
 
-  it('basic component', () => {
-    cy.getByTestId('basic').compareSnapshot('shape-basic', 0.0)
-  })
+  it('basic component', () => cy.getByTestId('basic').compareSnapshot('shape-basic', 0.0))
 
-  it('component variants', () => {
-    cy.compareSnapshot('shape-variants', 0.0)
-  })
+  it('component variants', () => cy.compareSnapshot('shape-variants', 0.0))
 })
