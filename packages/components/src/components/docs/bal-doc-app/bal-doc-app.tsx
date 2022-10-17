63b7c1784e454b2eeacc8325f65fc3ea9896f5e8
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-10-17 11:18:18 +0200

    test: wait until icons are loaded

--- packages/components/src/components/docs/bal-doc-app/bal-doc-app.tsx
+++ packages/components/src/components/docs/bal-doc-app/bal-doc-app.tsx
@@ -1,7 +1,13 @@
 import { Component, Host, h, Event, EventEmitter } from '@stencil/core'
 import * as balIcons from '@baloise/design-system-next-icons'
 import globalScript from '../../../global'
-import { updateBalIcons } from '../../../config'
+import {
+  attachComponentToConfig,
+  BalConfigObserver,
+  BalConfigState,
+  detachComponentToConfig,
+  updateBalIcons,
+} from '../../../config'
 
 @Component({
   tag: 'bal-doc-app',
@@ -9,20 +15,39 @@ import { updateBalIcons } from '../../../config'
   shadow: false,
   styleUrl: '../../../styles/global.sass',
 })
-export class DocApp {
+export class DocApp implements BalConfigObserver {
+  didLoad = false
+  didLoadIcons = false
+
   @Event({ bubbles: true, composed: true }) balAppLoad!: EventEmitter<boolean>
 
   connectedCallback() {
     globalScript()
+    attachComponentToConfig(this)
   }
 
-  balAppLoadTimer?: NodeJS.Timer
+  disconnectedCallback() {
+    detachComponentToConfig(this)
+  }
 
   componentDidLoad() {
     updateBalIcons(balIcons)
+    this.didLoad = true
+    this.notify()
+  }
 
-    clearTimeout(this.balAppLoadTimer)
-    this.balAppLoadTimer = setTimeout(() => this.balAppLoad.emit(true), 0)
+  configChanged(state: BalConfigState): void {
+    const hasIconsLoaded = Object.keys(state.icons).length > 0
+    if (hasIconsLoaded) {
+      this.didLoadIcons = true
+    }
+    this.notify()
+  }
+
+  notify() {
+    if (this.didLoad && this.didLoadIcons) {
+      this.balAppLoad.emit(true)
+    }
   }
 
   render() {
