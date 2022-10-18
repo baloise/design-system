a5b45cd18961f3d54c2daf1550d9d92891ff2c98
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-18 16:42:12 +0200

    test(bal-footer): refactor

--- packages/components/cypress/e2e/visual/bal-footer.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-footer.visual.cy.ts
@@ -2,13 +2,12 @@ describe('bal-footer', () => {
   before(() => cy.page('/components/bal-footer/test/bal-footer.visual.html'))
 
   it('basic component', () => {
-    cy.intercept({
-      method: 'GET',
-      url: 'https://www.baloise.ch/app-integration/footer-links/*',
-    }).as('getFooterLinks')
+    // cy.intercept({
+    //   method: 'GET',
+    //   url: '/app-integration/footer-links/*',
+    // }).as('getFooterLinks')
 
-    cy.wait('@getFooterLinks')
-
+    // cy.wait('@getFooterLinks')
     cy.platform('desktop')
     cy.getByTestId('basic').compareSnapshot('footer-desktop', 0.0)
 
