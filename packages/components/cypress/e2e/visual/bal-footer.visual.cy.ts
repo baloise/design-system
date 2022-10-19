c005d66337bd1b4817e6d3dc32e7305e3f8de039
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-19 11:07:12 +0200

    test(bal-footer): exclude links from the visual test

--- packages/components/cypress/e2e/visual/bal-footer.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-footer.visual.cy.ts
@@ -2,30 +2,24 @@ describe('bal-footer', () => {
   before(() => cy.page('/components/bal-footer/test/bal-footer.visual.html'))
 
   it('basic component', () => {
-    // cy.intercept({
-    //   method: 'GET',
-    //   url: '/app-integration/footer-links/*',
-    // }).as('getFooterLinks')
-
-    // cy.wait('@getFooterLinks')
     cy.platform('desktop')
     cy.getByTestId('basic').compareSnapshot('footer-desktop', 0.0)
 
-    // cy.platform('tablet') /
-    // cy.getByTestId('basic').compareSnapshot('footer-tablet', 0.0)
+    cy.platform('tablet')
+    cy.getByTestId('basic').compareSnapshot('footer-tablet', 0.0)
 
-    // cy.platform('mobile')
-    // cy.getByTestId('basic').compareSnapshot('footer-mobile', 0.0)
+    cy.platform('mobile')
+    cy.getByTestId('basic').compareSnapshot('footer-mobile', 0.0)
   })
 
-  // it('component variants', () => {
-  //   cy.platform('desktop')
-  //   cy.compareSnapshot('footer-variants-desktop', 0.0)
+  it('component variants', () => {
+    cy.platform('desktop')
+    cy.compareSnapshot('footer-variants-desktop', 0.0)
 
-  //   cy.platform('tablet')
-  //   cy.compareSnapshot('footer-variants-tablet', 0.0)
+    cy.platform('tablet')
+    cy.compareSnapshot('footer-variants-tablet', 0.0)
 
-  //   cy.platform('mobile')
-  //   cy.compareSnapshot('footer-variants-mobile', 0.0)
-  // })
+    cy.platform('mobile')
+    cy.compareSnapshot('footer-variants-mobile', 0.0)
+  })
 })
