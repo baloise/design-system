5c86147bbdf15996ae381047ba739d9bfc5d4264
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-20 15:54:46 +0200

    test(footer): refactor

--- packages/components/cypress/e2e/visual/bal-hint.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-hint.visual.cy.ts
@@ -4,57 +4,46 @@ describe('bal-hint', () => {
   it('basic component', () => {
     cy.platform('desktop')
     cy.getByTestId('basic').compareSnapshot('hint-basic', 0.0)
-  })
-
-  it('basic component open', () => {
-    cy.platform('desktop')
     cy.getByTestId('basic').click()
     cy.compareSnapshot('hint-basic-open-desktop', 0.0)
-
+    // close
     cy.platform('tablet')
     cy.getByTestId('basic').click()
     cy.compareSnapshot('hint-basic-open-tablet', 0.0)
-
+    // close
     cy.platform('mobile')
     cy.getByTestId('basic').click()
     cy.compareSnapshot('hint-basic-open-mobile', 0.0)
+    // close
   })
 
   it('basic component with close label', () => {
     cy.platform('desktop')
-    cy.getByTestId('close-label').compareSnapshot('hint-close-label', 0.0)
-  })
-
-  it('basic component open with close label', () => {
-    cy.platform('desktop')
-    cy.getByTestId('close-label').click()
+    cy.getByTestId('close-label-id').compareSnapshot('hint-close-label', 0.0)
+    cy.getByTestId('close-label-id').click()
     cy.compareSnapshot('hint-close-label-open-desktop', 0.0)
 
     cy.platform('tablet')
-    cy.getByTestId('close-label').click()
+    cy.getByTestId('close-label-id').click()
     cy.compareSnapshot('hint-close-label-open-tablet', 0.0)
 
     cy.platform('mobile')
-    cy.getByTestId('close-label').click()
+    cy.getByTestId('close-label-id').click()
     cy.compareSnapshot('hint-close-label-open-mobile', 0.0)
   })
 
   it('small component', () => {
     cy.platform('desktop')
-    cy.getByTestId('small').compareSnapshot('hint-small', 0.0)
-  })
-
-  it('small component', () => {
-    cy.platform('desktop')
-    cy.getByTestId('small').click()
+    cy.getByTestId('small-id').compareSnapshot('hint-small', 0.0)
+    cy.getByTestId('small-id').click()
     cy.compareSnapshot('hint-small-open-desktop', 0.0)
 
     cy.platform('tablet')
-    cy.getByTestId('small').click()
+    cy.getByTestId('small-id').click()
     cy.compareSnapshot('hint-small-open-tablet', 0.0)
 
     cy.platform('mobile')
-    cy.getByTestId('small').click()
+    cy.getByTestId('small-id').click()
     cy.compareSnapshot('hint-small-open-mobile', 0.0)
   })
 })
