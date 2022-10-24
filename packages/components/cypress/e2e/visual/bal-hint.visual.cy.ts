e0d4e79b2dc86c94d0442a8150c1b47a39676704
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-24 16:07:42 +0200

    test(hint): debugging

--- packages/components/cypress/e2e/visual/bal-hint.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-hint.visual.cy.ts
@@ -3,41 +3,27 @@ describe('bal-hint', () => {
 
   it('basic component', () => {
     cy.platform('desktop')
-    cy.getByTestId('basic').compareSnapshot('hint-basic', 0.0)
+    cy.getByTestId('basic').compareSnapshot('hint-basic-desktop', 0.0)
     cy.getByTestId('basic').click().balHintFindOverlay().compareSnapshot('hint-basic-open-desktop', 0.0)
-    cy.getByTestId('close-label').compareSnapshot('hint-close-label', 0.0)
+    cy.getByTestId('close-label').compareSnapshot('hint-close-label-desktop', 0.0)
     cy.getByTestId('close-label').click().balHintFindOverlay().compareSnapshot('hint-close-label-desktop', 0.0)
-    cy.getByTestId('small').compareSnapshot('hint-small', 0.0)
+    cy.getByTestId('small').compareSnapshot('hint-small-desktop', 0.0)
     cy.getByTestId('small').click().balHintFindOverlay().compareSnapshot('hint-small-desktop', 0.0)
+
+    cy.platform('tablet')
+    cy.getByTestId('basic').compareSnapshot('hint-basic-tablet', 0.0)
+    cy.getByTestId('basic').click().balHintFindOverlay().compareSnapshot('hint-basic-open-tablet', 0.0)
+    cy.getByTestId('close-label').compareSnapshot('hint-close-label-tablet', 0.0)
+    cy.getByTestId('close-label').click().balHintFindOverlay().compareSnapshot('hint-close-label-tablet', 0.0)
+    cy.getByTestId('small').compareSnapshot('hint-small-tablet', 0.0)
+    cy.getByTestId('small').click().balHintFindOverlay().compareSnapshot('hint-small-tablet', 0.0)
+
+    cy.platform('mobile')
+    cy.getByTestId('basic').compareSnapshot('hint-basic-mobile', 0.0)
+    cy.getByTestId('basic').click().balHintFindOverlay().compareSnapshot('hint-basic-open-mobile', 0.0)
+    cy.getByTestId('close-label').compareSnapshot('hint-close-label-mobile', 0.0)
+    cy.getByTestId('close-label').click().balHintFindOverlay().compareSnapshot('hint-close-label-mobile', 0.0)
+    cy.getByTestId('small').compareSnapshot('hint-small-mobile', 0.0)
+    cy.getByTestId('small').click().balHintFindOverlay().compareSnapshot('hint-small-mobile', 0.0)
   })
-
-  // it('basic component with close label', () => {
-  //   cy.platform('desktop')
-  //   cy.getByTestId('close-label-id').compareSnapshot('hint-close-label', 0.0)
-  //   cy.getByTestId('close-label-id').click()
-  //   cy.compareSnapshot('hint-close-label-open-desktop', 0.0)
-
-  //   cy.platform('tablet')
-  //   cy.getByTestId('close-label-id').click()
-  //   cy.compareSnapshot('hint-close-label-open-tablet', 0.0)
-
-  //   cy.platform('mobile')
-  //   cy.getByTestId('close-label-id').click()
-  //   cy.compareSnapshot('hint-close-label-open-mobile', 0.0)
-  // })
-
-  // it('small component', () => {
-  //   cy.platform('desktop')
-  //   cy.getByTestId('small-id').compareSnapshot('hint-small', 0.0)
-  //   cy.getByTestId('small-id').click()
-  //   cy.compareSnapshot('hint-small-open-desktop', 0.0)
-
-  //   cy.platform('tablet')
-  //   cy.getByTestId('small-id').click()
-  //   cy.compareSnapshot('hint-small-open-tablet', 0.0)
-
-  //   cy.platform('mobile')
-  //   cy.getByTestId('small-id').click()
-  //   cy.compareSnapshot('hint-small-open-mobile', 0.0)
-  // })
 })
