ed179c54630c01e424c39c0cbd4552d8648e1136
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-24 09:07:02 +0200

    test(icon): remove mobile and tablet

--- packages/components/cypress/e2e/visual/bal-icon.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-icon.visual.cy.ts
@@ -4,22 +4,10 @@ describe('bal-icon', () => {
   it('basic component', () => {
     cy.platform('desktop')
     cy.getByTestId('basic').compareSnapshot('icon-desktop', 0.0)
-
-    cy.platform('tablet')
-    cy.getByTestId('basic').compareSnapshot('icon-tablet', 0.0)
-
-    cy.platform('mobile')
-    cy.getByTestId('basic').compareSnapshot('icon-mobile', 0.0)
   })
 
   it('component variants', () => {
     cy.platform('desktop')
     cy.compareSnapshot('icon-variants-desktop', 0.0)
-
-    cy.platform('tablet')
-    cy.compareSnapshot('icon-variants-tablet', 0.0)
-
-    cy.platform('mobile')
-    cy.compareSnapshot('icon-variants-mobile', 0.0)
   })
 })
