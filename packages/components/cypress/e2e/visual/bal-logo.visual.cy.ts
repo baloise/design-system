30a5bb3c961dd36d75f74fcd1dcec074f96b7388
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-25 12:06:18 +0200

    test(logo): add visual test

---
+++ packages/components/cypress/e2e/visual/bal-logo.visual.cy.ts
@@ -0,0 +1,17 @@
+describe('bal-logo', () => {
+  before(() => cy.page('/components/bal-logo/test/bal-logo.visual.html'))
+
+  it('basic component', () => {
+    cy.platform('desktop')
+    cy.getByTestId('basic').compareSnapshot('logo-basic-desktop', 0.0)
+    cy.getByTestId('colors').compareSnapshot('logo-colors-desktop', 0.0)
+    cy.getByTestId('colors-inverted').compareSnapshot('logo-colors-inverted-desktop', 0.0)
+    cy.getByTestId('animated').compareSnapshot('logo-animated-desktop', 0.1)
+
+    // cy.platform('tablet')
+    // cy.getByTestId('basic').compareSnapshot('logo-basic-tablet', 0.0)
+
+    // cy.platform('mobile')
+    // cy.getByTestId('basic').compareSnapshot('logo-basic-mobile', 0.0)
+  })
+})
