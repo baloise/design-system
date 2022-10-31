73b4d8bc21ea48d30f38bd7286bed38f1d68f74c
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-31 11:18:16 +0100

    test(sheet): refactor

--- packages/components/cypress/e2e/visual/bal-sheet.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-sheet.visual.cy.ts
@@ -3,12 +3,12 @@ describe('bal-sheet', () => {
 
   it('basic component', () => {
     cy.platform('desktop')
-    cy.getByTestId('basic').compareSnapshot('sheet-basic-desktop', 0.0)
+    cy.compareSnapshot('sheet-basic-desktop', 0.0)
 
     cy.platform('tablet')
-    cy.getByTestId('basic').compareSnapshot('sheet-basic-tablet', 0.0)
+    cy.compareSnapshot('sheet-basic-tablet', 0.0)
 
     cy.platform('mobile')
-    cy.getByTestId('basic').compareSnapshot('sheet-basic-mobile', 0.0)
+    cy.compareSnapshot('sheet-basic-mobile', 0.0)
   })
 })
