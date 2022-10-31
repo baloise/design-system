a5199d63206c8dfe38c1bf9e691bdffd59605ed8
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-31 15:52:40 +0100

    test(hint): refactor

--- packages/components/cypress/e2e/visual/bal-hint.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-hint.visual.cy.ts
@@ -6,11 +6,7 @@ describe('bal-hint', () => {
     cy.getByTestId('basic').compareSnapshot('hint-basic-desktop', 0.0)
     cy.getByTestId('basic').click().balHintFindOverlay().compareSnapshot('hint-basic-open-desktop', 0.0)
     cy.getByTestId('basic').balHintFindCloseButton().click()
-    cy.getByTestId('close-label').compareSnapshot('hint-close-label-desktop', 0.0)
-    cy.getByTestId('close-label').click().balHintFindOverlay().compareSnapshot('hint-close-label-desktop', 0.0)
-    cy.getByTestId('close-label').balHintFindCloseButton().click()
-    // cy.getByTestId('small').compareSnapshot('hint-small-desktop', 0.0)
-    // cy.getByTestId('small').click().balHintFindOverlay().compareSnapshot('hint-small-desktop', 0.0)
+    cy.getByTestId('small').click().balHintFindOverlay().compareSnapshot('hint-small-desktop', 0.0)
 
     // cy.platform('tablet')
     // cy.getByTestId('basic').compareSnapshot('hint-basic-tablet', 0.0)
