6e6cc3dd4cd7bd9e45f112cecc09df46b69b11dc
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-11-16 22:13:06 +0100

    fix(logo): load svg on resize

--- packages/components/src/components/bal-logo/bal-logo.tsx
+++ packages/components/src/components/bal-logo/bal-logo.tsx
@@ -28,6 +28,7 @@ export class Logo implements ComponentInterface, Loggable {
   private animatedLogoElement!: HTMLDivElement
   private animationFunction?: LogoAnimationFunction
   private resizeWidthHandler = ResizeHandler()
+  private isTouch = isPlatform('touch')
 
   log!: LogInstance
 
@@ -39,16 +40,6 @@ export class Logo implements ComponentInterface, Loggable {
   @Element() el!: HTMLElement
 
   @State() logoSvg: any = ''
-  @State() isTouch = isPlatform('touch')
-
-  @Watch('isTouch')
-  touchWatcher() {
-    if (this.isTouch) {
-      this.loadSmallSVGLogo()
-    } else {
-      this.loadLargeSVGLogo()
-    }
-  }
 
   /**
    * PUBLIC PROPERTY API
@@ -68,7 +59,6 @@ export class Logo implements ComponentInterface, Loggable {
   animatedWatcher() {
     if (!this.animated) {
       this.destroyAnimation()
-      this.touchWatcher()
     }
   }
 
@@ -79,6 +69,7 @@ export class Logo implements ComponentInterface, Loggable {
 
   connectedCallback() {
     this.animatedWatcher()
+    this.loadSvgLogo()
   }
 
   componentDidUpdate() {
@@ -87,7 +78,7 @@ export class Logo implements ComponentInterface, Loggable {
 
   componentDidLoad() {
     this.resetAnimation()
-    this.touchWatcher()
+    this.loadSvgLogo()
   }
 
   disconnectedCallback() {
@@ -102,7 +93,11 @@ export class Logo implements ComponentInterface, Loggable {
   @Listen('resize', { target: 'window' })
   async resizeHandler() {
     this.resizeWidthHandler(() => {
-      this.isTouch = isPlatform('touch')
+      const newIsTouch = isPlatform('touch')
+      if (this.isTouch !== newIsTouch) {
+        this.isTouch = newIsTouch
+        this.loadSvgLogo()
+      }
     })
   }
 
@@ -110,6 +105,14 @@ export class Logo implements ComponentInterface, Loggable {
    * PRIVATE METHODS
    * ------------------------------------------------------
    */
+
+  private loadSvgLogo() {
+    if (this.isTouch) {
+      this.loadSmallSVGLogo()
+    } else {
+      this.loadLargeSVGLogo()
+    }
+  }
 
   private async resetAnimation() {
     this.destroyAnimation()
