42822c2fef0e747d8b568079c8f5c63462e74497
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-11-17 14:13:54 +0100

    test(popover): add visual test

---
+++ packages/components/cypress/e2e/visual/bal-popover.visual.cy.ts
@@ -0,0 +1,27 @@
+describe('bal-popover', () => {
+  testPopover('mobile')
+  testPopover('desktop')
+
+  function testPopover(platform: 'desktop' | 'mobile') {
+    describe(platform, () => {
+      before(() => cy.page('/components/bal-popover/test/bal-popover.visual.html').platform(platform))
+
+      it('basic component', () => {
+        cy.getByTestId('popover-trigger').click()
+        cy.compareSnapshot('popover-basic', 0.0)
+        cy.get('body').type('{esc}')
+      })
+
+      it('arrow backdrop component', () => {
+        cy.getByTestId('popover-arrow-trigger').click()
+        cy.compareSnapshot('popover-arrow', 0.0)
+        cy.get('body').type('{esc}')
+      })
+
+      it('tooltip component', () => {
+        cy.getByTestId('popover-tooltip-trigger').invoke('show').click({ force: true })
+        cy.compareSnapshot('popover-tooltip', 0.0)
+      })
+    })
+  }
+})
