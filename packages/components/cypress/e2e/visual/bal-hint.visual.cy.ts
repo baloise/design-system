63f2a04a3b0258ab431d8d1aacd885badb41b684
mladenplaninicic <mladenplaninicic@gmail.com>
2022-11-22 11:42:19 +0100

    chore(): trigger build

--- packages/components/cypress/e2e/visual/bal-hint.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-hint.visual.cy.ts
@@ -14,16 +14,16 @@ describe('bal-hint', () => {
   })
 
   it('basic component', () => {
-    // cy.platform('desktop')
-    // cy.getByTestId('basic').compareSnapshot('hint-basic-desktop', 0.0)
-    // cy.getByTestId('basic').click().balHintFindOverlay().compareSnapshot('hint-basic-open-desktop', 0.0)
-    // cy.getByTestId('basic').balHintFindCloseButton().click()
+    cy.platform('desktop')
+    cy.getByTestId('basic').compareSnapshot('hint-basic-desktop', 0.0)
+    cy.getByTestId('basic').click().balHintFindOverlay().compareSnapshot('hint-basic-open-desktop', 0.0)
+    cy.getByTestId('basic').balHintFindCloseButton().click()
     // cy.getByTestId('small').click().balHintFindOverlay().compareSnapshot('hint-small-desktop', 0.0)
 
-    // cy.platform('tablet')
-    // cy.getByTestId('basic').compareSnapshot('hint-basic-tablet', 0.0)
-    // cy.getByTestId('basic').click().balHintFindOverlay().compareSnapshot('hint-basic-open-tablet', 0.0)
-    // cy.getByTestId('basic').balHintFindCloseButton().click()
+    cy.platform('tablet')
+    cy.getByTestId('basic').compareSnapshot('hint-basic-tablet', 0.0)
+    cy.getByTestId('basic').click().balHintFindOverlay().compareSnapshot('hint-basic-open-tablet', 0.0)
+    cy.getByTestId('basic').balHintFindCloseButton().click()
     // cy.getByTestId('small').click().balHintFindOverlay().compareSnapshot('hint-small-tablet', 0.0)
 
     cy.platform('mobile')
