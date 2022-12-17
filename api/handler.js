1745d43a2f2bb8aa2909d9612e8d77bb52b3e880
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-12-17 20:06:05 +0100

    feat: add static file server for fonts

---
+++ api/handler.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const serve = require('serve-handler')
+
+export default function handler(request, response) {
+  return serve(request, response, {
+    public: './public',
+  })
+}
