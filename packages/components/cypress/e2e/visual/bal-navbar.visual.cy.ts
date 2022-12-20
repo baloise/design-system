53deb5e156b367266721e61f04d2c295d7c42533
mladenplaninicic <mladenplaninicic@gmail.com>
2022-12-20 11:22:13 +0100

    chore(bal-navbar): refactor

--- packages/components/cypress/e2e/visual/bal-navbar.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-navbar.visual.cy.ts
@@ -41,7 +41,7 @@ describe('bal-navbar', () => {
         cy.getByTestId('basic').compareSnapshot(`navbar-basic-${platform}`, 0.0)
         if (platform !== 'desktop') {
           cy.getByTestId('basic').find('.bal-navbar__brand__burger').click()
-          cy.getByTestId('basic').compareSnapshot(`navbar-basic-opened-${platform}`, 0.0)
+          cy.compareSnapshot(`navbar-basic-opened-${platform}`, 0.0)
           cy.getByTestId('basic').find('.bal-navbar__brand__burger').click()
         }
         cy.getByTestId('simple-light').compareSnapshot(`navbar-simple-light-${platform}`, 0.0)
