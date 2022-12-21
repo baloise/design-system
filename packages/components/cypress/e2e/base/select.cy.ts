514ee22a2f316e36dac7ec70dc9beba4f8676362
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-12-21 09:53:43 +0100

    test(select): add form reset

--- packages/components/cypress/e2e/base/select.cy.ts
+++ packages/components/cypress/e2e/base/select.cy.ts
@@ -20,7 +20,7 @@ describe('bal-select', () => {
     cy.getByTestId('select-disabled').should('be.disabled')
   })
 
-  describe.only('typeahead', () => {
+  describe('typeahead', () => {
     it('should clear select and search for the Black Widow', () => {
       cy.getByTestId('typeahead').clear().blur()
       cy.getByTestId('typeahead').type('Black{downArrow}{enter}').blur().should('have.value', 'Black Widow')
@@ -28,7 +28,7 @@ describe('bal-select', () => {
   })
 
   describe('multiple', () => {
-    it('should ', () => {
+    it('should select and deselect values', () => {
       cy.getByTestId('multiple')
         .click()
         .select(['Black Widow', 'Black Panter'])
@@ -39,6 +39,37 @@ describe('bal-select', () => {
       cy.getByTestId('multiple').should('have.value', ['Iron Man'])
       cy.getByTestId('multiple').balSelectFindChips().first().contains('Iron Man').click()
       cy.getByTestId('multiple').should('have.value', '')
+      cy.get('body').click()
+    })
+  })
+
+  describe('form reset', () => {
+    it('should restore initial value when it has been reset', () => {
+      // First we change the initial values to 1995
+      cy.getByTestId('reset-basic').click().select('1995')
+      cy.getByTestId('reset-typeahead').clear().type('1995{downArrow}{enter}')
+      cy.getByTestId('reset-multiple').click().balSelectFindOptions().first().click()
+      cy.getByTestId('reset-multiple').balSelectFindOptions().eq(1).click()
+      cy.getByTestId('reset-multiple').balSelectFindOptions().eq(2).click()
+      cy.getByTestId('reset-multiple-typeahead').click({ force: true }).click().balSelectFindOptions().first().click()
+      cy.getByTestId('reset-multiple-typeahead').balSelectFindOptions().eq(1).click()
+      cy.getByTestId('reset-multiple-typeahead').balSelectFindOptions().eq(2).click()
+      cy.getByTestId('reset-typeahead-remote').click({ force: true }).click().balSelectFindOptions().first().click()
+
+      // Values are changed form the initial one
+      cy.getByTestId('reset-basic').should('have.value', '1995')
+      cy.getByTestId('reset-typeahead').should('have.value', '1995')
+      cy.getByTestId('reset-multiple').should('have.value', ['1995'])
+      cy.getByTestId('reset-multiple-typeahead').should('have.value', ['1995'])
+      cy.getByTestId('reset-typeahead-remote').should('have.value', '1995')
+
+      // Reset form and check if initial value is restored
+      cy.getByTestId('reset-button').click()
+      cy.getByTestId('reset-basic').should('have.value', '1996')
+      cy.getByTestId('reset-typeahead').should('have.value', '1996')
+      cy.getByTestId('reset-multiple').should('have.value', ['1996', '1997'])
+      cy.getByTestId('reset-multiple-typeahead').should('have.value', ['1996', '1997'])
+      cy.getByTestId('reset-typeahead-remote').should('have.value', '1996')
     })
   })
 })
