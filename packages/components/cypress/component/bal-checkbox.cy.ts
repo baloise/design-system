83894031c9495b565eff894ebc9c6b04c89390eb
mladenplaninicic <mladenplaninicic@gmail.com>
2023-02-10 15:52:15 +0100

    fix(bal-checkbox): fix focus and blur events

--- packages/components/cypress/component/bal-checkbox.cy.ts
+++ packages/components/cypress/component/bal-checkbox.cy.ts
@@ -1,44 +1,60 @@
 import { BalCheckbox } from '../../.storybook/vue/generated/components'
 
 describe('bal-checkbox.cy.ts', () => {
-  it('should have a default slot', () => {
-    cy.mount(BalCheckbox, { slots: { default: () => 'My label' } })
-    cy.get('bal-checkbox').contains('My label')
-    cy.get('bal-checkbox').find('input').should('not.be.checked')
-  })
+  let onClickSpy: Cypress.Agent<sinon.SinonSpy>
+  let onBalChangeSpy: Cypress.Agent<sinon.SinonSpy>
+  let onBalInputSpy: Cypress.Agent<sinon.SinonSpy>
+  let onBalFocusSpy: Cypress.Agent<sinon.SinonSpy>
+  let onBalBlurSpy: Cypress.Agent<sinon.SinonSpy>
 
-  it('should fire change event', () => {
-    const onBalChangeSpy = cy.spy().as('balChange')
+  beforeEach(() => {
+    onClickSpy = cy.spy().as('click')
+    onBalChangeSpy = cy.spy().as('balChange')
+    onBalInputSpy = cy.spy().as('balInput')
+    onBalFocusSpy = cy.spy().as('balFocus')
+    onBalBlurSpy = cy.spy().as('balBlur')
 
-    cy.mount(BalCheckbox, {
+    cy.mount(BalCheckbox as any, {
       props: {
+        onClick: onClickSpy,
+        onBalInput: onBalInputSpy,
         onBalChange: onBalChangeSpy,
+        onBalFocus: onBalFocusSpy,
+        onBalBlur: onBalBlurSpy,
       },
       slots: {
         default: () => 'My label',
       },
     })
+  })
 
+  it('should have a default slot', () => {
+    cy.get('bal-checkbox').contains('My label')
+    cy.get('bal-checkbox').find('input').should('not.be.checked')
+  })
+
+  it('should fire change and focus events', () => {
     cy.get('bal-checkbox').click()
     cy.get('bal-checkbox').find('input').should('be.checked')
     cy.get('@balChange').should('have.been.calledOnce')
+    cy.get('@balFocus').should('have.been.calledOnce')
   })
 
-  it('should not fire change event', () => {
-    const onBalChangeSpy = cy.spy().as('balChange')
+  it('should fire blur event after leaving the field', () => {
+    cy.get('bal-checkbox').click().get('.bal-radio-checkbox__input').blur()
+    cy.get('@balBlur').should('have.been.calledOnce')
+  })
 
+  it('should not fire change and focus events because checkbox is disabled', () => {
     cy.mount(BalCheckbox, {
       props: {
         disabled: true,
-        onBalChange: onBalChangeSpy,
-      },
-      slots: {
-        default: () => 'My label',
       },
     })
 
     cy.get('bal-checkbox').click({ force: true })
     cy.get('bal-checkbox').find('input').should('not.be.checked')
     cy.get('@balChange').should('not.have.been.called')
+    cy.get('@balFocus').should('not.have.been.calledOnce')
   })
 })
