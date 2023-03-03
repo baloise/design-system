b260308dc224cffcacd739d5868778a5ed4fb0dc
mladenplaninicic <mladenplaninicic@gmail.com>
2023-03-03 14:20:45 +0100

    chore(): refactor

--- packages/components/src/components/notice/bal-modal/bal-modal.type.ts
+++ packages/components/src/components/notice/bal-modal/bal-modal.type.ts
@@ -16,7 +16,7 @@ export interface ModalOptions<T extends ComponentRef = ComponentRef> {
   modalWidth?: number
   cssClass?: string | string[]
   isClosable?: boolean
-  backdropDissmis?: boolean
+  backdropDismiss?: boolean
   hasBackdrop?: boolean
   dataTestId?: string
   delegate?: FrameworkDelegate
