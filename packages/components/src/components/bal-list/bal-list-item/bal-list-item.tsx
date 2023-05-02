c1863539771759ca2883357893633da5d86b17dc
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-05-02 13:06:40 +0200

    chore: remove duplicated prop

--- packages/components/src/components/bal-list/bal-list-item/bal-list-item.tsx
+++ packages/components/src/components/bal-list/bal-list-item/bal-list-item.tsx
@@ -83,14 +83,6 @@ export class ListItem implements ComponentInterface, BalConfigObserver, Loggable
   @Prop() href = ''
 
   /**
-   * This attribute instructs browsers to download a URL instead of navigating to
-   * it, so the user will be prompted to save it as a local file. If the attribute
-   * has a value, it is used as the pre-filled file name in the Save prompt
-   * (the user can still change the file name if they want).
-   */
-  @Prop() download?: string
-
-  /**
    * Specifies where to open the linked document
    */
   @Prop() target: BalProps.BalListItemTarget = '_self'
