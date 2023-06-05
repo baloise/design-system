9b14d6840dc01fd31d7688c442d8101e61edd687
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-06-05 13:28:16 +0200

    fix: load contact js after first render

--- packages/components/src/utils/swipe/swipe.listener.ts
+++ packages/components/src/utils/swipe/swipe.listener.ts
@@ -1,19 +1,37 @@
-import { PointerListener } from 'contactjs'
+import type { PointerListener } from 'contactjs'
 import { ListenerAbstract } from '../types/listener'
 import { BalSwipeInfo } from './swipe.interfaces'
+import { rIC } from '../helpers'
 
 export class BalSwipeListener<TObserver> extends ListenerAbstract<TObserver, BalSwipeInfo> {
+  private PointerListenerLib: typeof PointerListener | undefined
   private pointerListener: PointerListener | undefined
 
-  connect(el: HTMLElement): void {
+  async connect(el: HTMLElement): Promise<void> {
     super.connect(el)
-    this.pointerListener = new PointerListener(el, {})
-    this.pointerListener.on('swipeleft', () => this.notify({ left: true, right: false }))
-    this.pointerListener.on('swiperight', () => this.notify({ left: false, right: true }))
+    await this.loadLib()
+    if (this.PointerListenerLib) {
+      this.pointerListener = new this.PointerListenerLib(el, {})
+      this.pointerListener.on('swipeleft', () => this.notify({ left: true, right: false }))
+      this.pointerListener.on('swiperight', () => this.notify({ left: false, right: true }))
+    }
   }
 
   disconnect(): void {
     super.disconnect()
     this.pointerListener?.destroy()
+  }
+
+  private async loadLib(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      rIC(async () => {
+        import('contactjs')
+          .then(module => {
+            this.PointerListenerLib = module.PointerListener
+            resolve()
+          })
+          .catch(reject)
+      })
+    })
   }
 }
