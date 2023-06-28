02051477da7fa3da32923741e62251a9b93492ce
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-06-28 09:42:10 +0200

    test: update popup tests

--- packages/components/cypress/e2e/visual/bal-popup.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-popup.visual.cy.ts
@@ -1,8 +1,4 @@
 describe('bal-popup', () => {
-  beforeEach(() => {
-    cy.visit('/components/bal-popup/test/bal-popup.visual.html').platform('desktop').waitForDesignSystem().wait(32)
-  })
-
   function testPopup(name: string, platform: 'mobile' | 'desktop' = 'desktop') {
     cy.compareSnapshot(`popup-${name}-${platform}`, 0.0)
     cy.getByTestId(`${name}-trigger`).click()
@@ -10,65 +6,66 @@ describe('bal-popup', () => {
     cy.getByTestId(`${name}-trigger`).click()
   }
 
-  it('basic component desktop', () => {
-    testPopup('basic')
-    testPopup('basic-backdrop-arrow')
-    testPopup('basic-backdrop-offset')
-  })
-
-  it('placement property desktop', () => {
-    testPopup('placement-right')
-    testPopup('placement-left')
-    testPopup('placement-top')
-    testPopup('placement-bottom')
-  })
-
-  it('variant property desktop', () => {
-    testPopup('fullscreen')
-    testPopup('drawer')
-  })
-
-  it('tabs combination desktop', () => {
-    testPopup('tabs')
-  })
-
-  // testRadioButton('mobile')
-  // testRadioButton('desktop')
-
-  // function testRadioButton(platform: 'mobile' | 'desktop') {
-  //   context(platform, () => {
-  //     beforeEach(() =>
-  //       cy.visit('/components/bal-popup/test/bal-popup.visual.html').platform(platform).waitForDesignSystem().wait(32),
-  //     )
-
-  //     function testPopup(name: string) {
-  //       cy.compareSnapshot(`popup-${name}-${platform}`, 0.0)
-  //       cy.getByTestId(`${name}-trigger`).click()
-  //       cy.compareSnapshot(`popup-${name}-${platform}-open`, 0.0)
-  //       cy.getByTestId(`${name}-trigger`).click()
-  //     }
-
-  //     it('basic component ' + platform, () => {
-  //       testPopup('basic')
-  //       testPopup('basic-backdrop-arrow')
-  //       testPopup('basic-backdrop-offset')
-  //     })
-
-  //     it('placement property ' + platform, () => {
-  //       testPopup('placement-right')
-  //       testPopup('placement-left')
-  //       testPopup('placement-top')
-  //       testPopup('placement-bottom')
-  //     })
-
-  //     it('variant property ' + platform, () => {
-  //       testPopup('fullscreen')
-  //       testPopup('drawer')
-  //     })
-
-  //     it('tabs combination ' + platform, () => {
-  //       testPopup('tabs')
-  //     })
+  // context('mobile', () => {
+  //   beforeEach(() => {
+  //     cy.visit('/components/bal-popup/test/bal-popup.visual.html').platform('mobile').waitForDesignSystem().wait(32)
   //   })
-  // }
+
+  //   it('basic component mobile', () => {
+  //     testPopup('basic', 'mobile')
+  //     testPopup('basic-backdrop-arrow', 'mobile')
+  //     testPopup('basic-backdrop-offset', 'mobile')
+  //   })
+
+  //   it('placement property mobile', () => {
+  //     testPopup('placement-right', 'mobile')
+  //     testPopup('placement-left', 'mobile')
+  //     testPopup('placement-top', 'mobile')
+  //     testPopup('placement-bottom', 'mobile')
+  //   })
+
+  //   it('variant property mobile', () => {
+  //     testPopup('fullscreen', 'mobile')
+  //     testPopup('drawer', 'mobile')
+  //   })
+
+  //   it('tabs combination mobile', () => {
+  //     testPopup('tabs', 'mobile')
+  //   })
+  // })
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.visit('/components/bal-popup/test/bal-popup.visual.html').platform('desktop').waitForDesignSystem().wait(32)
+    })
+
+    it('basic component desktop', () => {
+      testPopup('basic')
+      testPopup('basic-backdrop-arrow')
+      testPopup('basic-backdrop-offset')
+    })
+
+    it('placement property desktop', () => {
+      testPopup('placement-right')
+      testPopup('placement-left')
+      testPopup('placement-top')
+      testPopup('placement-bottom')
+    })
+
+    it('tabs combination desktop', () => {
+      testPopup('tabs')
+    })
+
+    it('variant property desktop', () => {
+      cy.compareSnapshot(`popup-fullscreen-desktop`, 0.0)
+      cy.getByTestId(`fullscreen-trigger`).click()
+      cy.compareSnapshot(`popup-fullscreen-desktop-open`, 0.0)
+      cy.get('body').type('{esc}').wait(32)
+
+      cy.compareSnapshot(`popup-drawer-desktop`, 0.0)
+      cy.getByTestId(`drawer-trigger`).click()
+      cy.compareSnapshot(`popup-drawer-desktop-open`, 0.0)
+      cy.get('body').type('{esc}').wait(32)
+    })
+  })
 })
