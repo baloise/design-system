85cb4e05829ad035c2275b41dbcf3c1b7252d685
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-07-06 08:35:57 +0200

    chore: merge branch 'main' into refactor/issue-911

--- packages/components/src/components/bal-spinner/bal-spinner.tsx
+++ packages/components/src/components/bal-spinner/bal-spinner.tsx
@@ -149,10 +149,6 @@ export class Spinner implements ComponentInterface, Loggable {
    */
 
   render() {
-<<<<<<< HEAD
-    return <Host aria-hidden="true" style={{ width: this.small ? '32px' : '64px' }}></Host>
-=======
-    return <Host role="progressbar" style={{ width: this.small ? '32px' : '64px' }}></Host>
->>>>>>> main
+    return <Host role="progressbar" aria-hidden="true" style={{ width: this.small ? '32px' : '64px' }}></Host>
   }
 }
