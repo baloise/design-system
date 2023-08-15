788460c0e7a39c287d7a3c3d26faa317d72242f3
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-08-15 09:18:31 +0200

    chore: add www again

--- packages/components/stencil.config.ts
+++ packages/components/stencil.config.ts
@@ -29,22 +29,22 @@ export const config: Config = {
     /**
      * Copy assets for E2E testing
      */
-    // {
-    //   type: 'www',
-    //   dir: 'www',
-    //   serviceWorker: false,
-    //   empty: true,
-    //   copy: [
-    //     {
-    //       src: '**/*.html',
-    //     },
-    //     {
-    //       src: 'components.d.ts',
-    //     },
-    //     { src: '../../css/css/baloise-design-system.css', dest: 'assets/baloise-design-system.css', warn: true },
-    //     { src: '../../fonts/lib', dest: 'assets/fonts', warn: true },
-    //     { src: '../public/assets/images', dest: 'assets/images', warn: true },
-    //   ],
-    // },
+    {
+      type: 'www',
+      dir: 'www',
+      serviceWorker: false,
+      empty: true,
+      copy: [
+        {
+          src: '**/*.html',
+        },
+        {
+          src: 'components.d.ts',
+        },
+        { src: '../../css/css/baloise-design-system.css', dest: 'assets/baloise-design-system.css', warn: true },
+        { src: '../../fonts/lib', dest: 'assets/fonts', warn: true },
+        { src: '../public/assets/images', dest: 'assets/images', warn: true },
+      ],
+    },
   ],
 }
