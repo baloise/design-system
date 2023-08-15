be813717d9e31227d22ae54c20fead37f92cfa4e
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-08-15 14:19:26 +0200

    chore: create build for e2e

--- packages/components/stencil.config.e2e.ts
+++ packages/components/stencil.config.e2e.ts
@@ -1,31 +1,12 @@
 import { Config } from '@stencil/core'
 
 import { StencilBaseConfig } from './config/stencil.basic.config'
-import { VueGenerator } from './config/stencil.bindings.vue'
 
 export const config: Config = {
   ...StencilBaseConfig,
   tsconfig: 'tsconfig.docs.json',
   outputTargets: [
     ...(StencilBaseConfig.outputTargets as any),
-    VueGenerator('../', '../../test/generated/components/index.ts', []),
-    // {
-    //   type: 'www',
-    //   dir: 'www',
-    //   serviceWorker: false,
-    //   empty: true,
-    //   copy: [
-    //     {
-    //       src: '**/*.html',
-    //     },
-    //     {
-    //       src: 'components.d.ts',
-    //     },
-    //     { src: '../../css/css/baloise-design-system.css', dest: 'assets/baloise-design-system.css', warn: true },
-    //     { src: '../../fonts/lib', dest: 'assets/fonts', warn: true },
-    //     { src: '../public/assets/images', dest: 'assets/images', warn: true },
-    //   ],
-    // },
     {
       type: 'www',
       dir: '../../test/generated/www',
