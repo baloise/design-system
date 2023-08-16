7c30f88c763b46b1b9308f71a40cbfa7402d9b03
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-08-16 08:32:29 +0200

    chore: adjust cypress config

--- apps/angular/cypress.config.ts
+++ apps/angular/cypress.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: true,
+  trashAssetsBeforeRuns: true,
+
+  viewportWidth: 1024,
+  viewportHeight: 1280,
+
+  env: {
+    failSilently: false,
+  },
+
+  includeShadowDom: true,
+
   e2e: {
     baseUrl: 'http://localhost:4200/',
     // setupNodeEvents(on, config) {
