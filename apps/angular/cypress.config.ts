f827b2c792ddd94b39592558edf5f3b8125de147
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-08-16 10:48:13 +0200

    chore: remove comments

--- apps/angular/cypress.config.ts
+++ apps/angular/cypress.config.ts
@@ -16,8 +16,5 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:4200/',
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
   },
 })
