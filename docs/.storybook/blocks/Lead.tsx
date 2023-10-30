9d02739a060caf8411411634d3a222e30711dfdb
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-10-30 16:48:41 +0100

    docs: upgrade icons

--- docs/.storybook/blocks/Lead.tsx
+++ docs/.storybook/blocks/Lead.tsx
@@ -4,7 +4,6 @@ export const Lead = ({ children }) => {
   return (
     <span className="sb-unstyled is-size-large has-text-primary my-x-large">
       {children}
-      <br />
     </span>
   )
 }
--- docs/stories/components/bal-icon/bal-icon.stories.ts
+++ docs/stories/components/bal-icon/bal-icon.stories.ts
@@ -1,12 +1,12 @@
-
 import type { JSX } from '@baloise/design-system-components'
+import { balBrandIconCarCrashWithAnimalGreen } from '@baloise/design-system-brand-icons'
 import type { Meta } from '@storybook/html'
 import { props, withRender, withContent, withDefaultContent, withComponentControls, StoryFactory } from '../../utils'
 
 type Args = JSX.BalIcon & { content: string }
 
 const meta: Meta<Args> = {
-  title: 'Components/Icon',
+  title: 'Components/Data View/Icon',
   args: {
     ...withDefaultContent(),
   },
@@ -26,10 +26,134 @@ export default meta
 
 const Story = StoryFactory<Args>(meta)
 
-export const Basic = Story()
-
-export const Secondary = Story({
+export const Basic = Story({
   args: {
-    // place props here
+    name: 'info-circle',
+    size: 'large',
+    content: '',
   },
 })
+
+export const UiIcons = Story({
+  args: {
+    name: 'info-circle',
+    size: 'large',
+    content: '',
+  },
+  ...withRender(
+    ({ content, ...args }) =>
+      `<div class="is-flex fg-medium is-flex-wrap-wrap">
+      ${[
+        'caret-down',
+        'caret-left',
+        'caret-up',
+        'check',
+        'close',
+        'date',
+        'document',
+        'edit',
+        'facebook',
+        'info-circle',
+        'instagram',
+        'linkedin',
+        'menu-bars',
+        'minus',
+        'nav-go-down',
+        'nav-go-left',
+        'nav-go-right',
+        'nav-go-up',
+        'plus',
+        'trash',
+        'twitter',
+        'upload',
+        'web',
+        'x',
+        'xing',
+        'youtube',
+      ]
+        .map(
+          item => `<div class="is-flex fg-small is-flex-wrap-wrap is-align-items-center" style="min-width: 180px">
+      <bal-icon name="${item}"></bal-icon>
+      <span>${item}</span>
+    </div>`,
+        )
+        .join(' ')}
+</div>`,
+  ),
+})
+
+// Youtube
+// Xing
+// X
+// Web
+// Upload
+// Twitter
+// Trash
+// Plus
+// NavGoUp
+// NavGoRight
+// NavGoLeft
+// NavGoDown
+// Minus
+// MenuBars
+// Linkedin
+// Instagram
+// InfoCircle
+// Facebook
+// Edit
+// Document
+// Date
+// Close
+// Check
+// CaretUp
+// CaretLeft
+// CaretDown
+
+export const BrandIcons = Story({
+  args: {
+    size: 'x-large',
+    color: 'auto',
+    content: '',
+  },
+  ...withRender(
+    ({ content, ...args }) =>
+      `<bal-icon ${props(args)} svg='${balBrandIconCarCrashWithAnimalGreen}'>${content}</bal-icon>`,
+  ),
+})
+
+// export const Sizes = Story({
+//   args: {
+//     name: 'info-circle',
+//     size: 'large',
+//     content: '',
+//   },
+//   ...withRender(
+//     () => `<div>
+//   <bal-icon name="date" size="x-small"></bal-icon>
+//   <bal-icon name="date" size="small"></bal-icon>
+//   <bal-icon name="date"></bal-icon>
+//   <bal-icon name="date" size="medium"></bal-icon>
+//   <bal-icon name="date" size="large"></bal-icon>
+//   <bal-icon name="date" size="x-large"></bal-icon>
+//   <bal-icon name="date" size="xx-large"></bal-icon>
+// </div>`,
+//   ),
+// })
+
+// export const Colors = Story({
+//   args: {
+//     name: 'info-circle',
+//     size: 'large',
+//     color: 'primary',
+//     content: '',
+//   },
+//   ...withRender(
+//     () => `<div>
+//   <bal-icon color="primary" name="github"></bal-icon>
+//   <bal-icon color="grey" name="github"></bal-icon>
+//   <bal-icon color="success" name="github"></bal-icon>
+//   <bal-icon color="warning" name="github"></bal-icon>
+//   <bal-icon color="danger" name="github"></bal-icon>
+// </div>`,
+//   ),
+// })
