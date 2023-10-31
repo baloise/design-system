ab479b7e8332ce37ff55fd9599200ad6117f0999
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-10-31 06:58:31 +0100

    Feat/storybook icons (#1093)
    * docs: upgrade icons
    
    * docs: upgrade icons
    
    * chore: format files

--- .build/docs-copy.js
+++ .build/docs-copy.js
@@ -14,6 +14,7 @@ const PACKAGES_COMPONENTS = path.join(PACKAGES, "components");
 const PACKAGES_MAPS = path.join(PACKAGES, "maps");
 const PACKAGES_CSS = path.join(PACKAGES, "css");
 const PACKAGES_ICONS = path.join(PACKAGES, "icons");
+const PACKAGES_BRAND_ICONS = path.join(PACKAGES, "brand-icons");
 const PACKAGES_FAVICONS = path.join(PACKAGES, "favicons");
 const PACKAGES_FONTS = path.join(PACKAGES, "fonts");
 const PACKAGES_TABLE = path.join(PACKAGES, "components-table");
@@ -37,6 +38,7 @@ async function main() {
 
   // data
   await copyToAsset(path.join(PACKAGES_ICONS, 'dist/icons.json'), 'assets/data/icons.json')
+  await copyToAsset(path.join(PACKAGES_BRAND_ICONS, 'dist/brand-icons.json'), 'assets/data/brand-icons.json')
   await copyToAsset(path.join(PACKAGES_COMPONENTS, '.tmp/commands.json'), 'assets/data/commands.json')
   await copyToAsset(path.join(PACKAGES_COMPONENTS, '.tmp/components.json'), 'assets/data/components.json')
   await copyToAsset(path.join(PACKAGES_COMPONENTS, '.tmp/components.d.ts'), 'assets/data/components.d.ts')
@@ -47,6 +49,10 @@ async function main() {
   // stylesheets
   await copyToPublic(path.join(PACKAGES_TABLE, 'css/design-system-table.css'), 'assets/css/design-system-table.css')
   await copyToPublic(path.join(PACKAGES_CSS, 'css/baloise-design-system.min.css'), 'assets/css/baloise-design-system.min.css')
+
+  // icons
+  await copyToPublic(path.join(PACKAGES_ICONS, 'svg'), 'assets/images/icons')
+  await copyToPublic(path.join(PACKAGES_BRAND_ICONS, 'svg'), 'assets/images/brand-icons')
 
   log.success('Resources are copied')
 }
--- docs/stories/components/bal-icon/theming.md
+++ docs/stories/components/bal-icon/theming.md
@@ -12,20 +12,22 @@ The component can be customization by changing the CSS variables.
 
 ### Variables​
 
-| Variable                         |
-| -------------------------------- |
-| `--bal-icon-color-grey`          |
-| `--bal-icon-color-grey-light`    |
-| `--bal-icon-color-success`       |
-| `--bal-icon-color-success-dark`  |
-| `--bal-icon-color-warning`       |
-| `--bal-icon-color-warning-dark`  |
-| `--bal-icon-color-danger`        |
-| `--bal-icon-color-danger-dark`   |
-| `--bal-icon-color-danger-darker` |
-| `--bal-icon-color-primary`       |
-| `--bal-icon-color-primary-dark`  |
-| `--bal-icon-color-blue`          |
-| `--bal-icon-color-light-blue`    |
-| `--bal-icon-color-primary-light` |
-| `--bal-icon-color-white`         |
+| Variable                          |
+| --------------------------------- |
+| `--bal-icon-color-grey`           |
+| `--bal-icon-color-grey-light`     |
+| `--bal-icon-color-success`        |
+| `--bal-icon-color-success-dark`   |
+| `--bal-icon-color-success-darker` |
+| `--bal-icon-color-warning`        |
+| `--bal-icon-color-warning-dark`   |
+| `--bal-icon-color-warning-darker` |
+| `--bal-icon-color-danger`         |
+| `--bal-icon-color-danger-dark`    |
+| `--bal-icon-color-danger-darker`  |
+| `--bal-icon-color-primary`        |
+| `--bal-icon-color-primary-dark`   |
+| `--bal-icon-color-blue`           |
+| `--bal-icon-color-light-blue`     |
+| `--bal-icon-color-primary-light`  |
+| `--bal-icon-color-white`          |
