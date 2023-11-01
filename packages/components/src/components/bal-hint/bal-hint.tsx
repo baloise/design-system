f4005de0f8ced73a3d36c7f3a763c1ad8ec4692a
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-11-01 13:26:50 +0100

    chore: add overview (#1098)

--- packages/components/src/components/bal-hint/bal-hint.tsx
+++ packages/components/src/components/bal-hint/bal-hint.tsx
@@ -14,6 +14,7 @@ import { BEM } from '../../utils/bem'
 import { preventDefault } from '../form/bal-select/utils/utils'
 import { BalScrollHandler } from '../../utils/scroll'
 import { ListenToBreakpoints, BalBreakpointObserver, BalBreakpoints, balBreakpoints } from '../../utils/breakpoints'
+import { balBrowser } from '../../utils/browser'
 
 @Component({
   tag: 'bal-hint',
@@ -27,6 +28,7 @@ export class Hint implements ComponentInterface, BalConfigObserver, BalBreakpoin
   private popoverElement!: HTMLBalPopoverElement
   private slotWrapperEl?: HTMLDivElement
   private hintContentEl?: HTMLDivElement
+  private docIntervalTimer?: NodeJS.Timer
 
   private bodyScrollHandler = new BalScrollHandler()
 
@@ -43,8 +45,22 @@ export class Hint implements ComponentInterface, BalConfigObserver, BalBreakpoin
    */
   @Prop() small = false
 
+  /**
+   * @internal
+   */
+  @Prop() demo = false
+
   connectedCallback() {
     this.bodyScrollHandler.connect()
+
+    if (balBrowser.hasDocument && this.demo) {
+      this.docIntervalTimer = setInterval(() => {
+        this.toggle()
+        if (this.demo === false) {
+          clearInterval(this.docIntervalTimer)
+        }
+      }, 2000)
+    }
   }
 
   componentDidRender() {
@@ -53,6 +69,7 @@ export class Hint implements ComponentInterface, BalConfigObserver, BalBreakpoin
 
   disconnectedCallback() {
     this.bodyScrollHandler.disconnect()
+    clearInterval(this.docIntervalTimer)
   }
 
   @ListenToBreakpoints()
