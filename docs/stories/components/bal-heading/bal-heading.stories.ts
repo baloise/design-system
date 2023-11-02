0fc2d28da507b2f8a7ee2ba8be64cdff44dd7d3f
mladenplaninicic <mladenplaninicic@gmail.com>
2023-11-02 14:38:36 +0100

    chore(): add typo docs

--- docs/stories/components/bal-heading/bal-heading.stories.ts
+++ docs/stories/components/bal-heading/bal-heading.stories.ts
@@ -1,4 +1,3 @@
-
 import type { JSX } from '@baloise/design-system-components'
 import type { Meta } from '@storybook/html'
 import { props, withRender, withContent, withDefaultContent, withComponentControls, StoryFactory } from '../../utils'
@@ -6,9 +5,9 @@ import { props, withRender, withContent, withDefaultContent, withComponentContro
 type Args = JSX.BalHeading & { content: string }
 
 const meta: Meta<Args> = {
-  title: 'Components/Heading',
+  title: 'Components/Typography/Heading',
   args: {
-    ...withDefaultContent(),
+    ...withDefaultContent('Heading'),
   },
   argTypes: {
     ...withContent(),
@@ -26,10 +25,99 @@ export default meta
 
 const Story = StoryFactory<Args>(meta)
 
-export const Basic = Story()
-
-export const Secondary = Story({
+export const Basic = Story({
   args: {
-    // place props here
+    level: 'h1',
+    subtitle: false,
+    space: 'bottom',
+    color: '',
+    inverted: false,
   },
+})
+
+export const Subtitle = Story({
+  args: {
+    content: 'Subtitle',
+    level: 'h3',
+    subtitle: true,
+    space: 'bottom',
+    color: '',
+    inverted: false,
+  },
+})
+
+export const Levels = Story({
+  ...withRender(
+    () => `<div class="columns">
+  <div class="column is-half has-background-yellow-1">
+    <bal-heading level="h1">Heading 1</bal-heading>
+    <bal-heading level="h2">Heading 2</bal-heading>
+    <bal-heading level="h3">Heading 3</bal-heading>
+    <bal-heading level="h4">Heading 4</bal-heading>
+    <bal-heading level="h5">Heading 5</bal-heading>
+  </div>
+  <div class="column is-half has-background-yellow-2">
+    <h1 class="title is-size-xxx-large">Heading 1</h1>
+    <h2 class="title is-size-xx-large">Heading 2</h2>
+    <h3 class="title is-size-x-large">Heading 3</h3>
+    <h4 class="title is-size-large">Heading 4</h4>
+    <h5 class="title is-size-normal">Heading 5</h5>
+  </div>
+</div>`,
+  ),
+})
+
+export const Colors = Story({
+  ...withRender(
+    () => `<div>
+    <bal-heading level="h4" space="none">Default / Primary</bal-heading>
+    <bal-heading color="success" level="h4" space="none">Success</bal-heading>
+    <bal-heading color="warning" level="h4" space="none">Warning</bal-heading>
+    <bal-heading color="danger" level="h4" space="none">Danger</bal-heading>
+  </div>`,
+  ),
+})
+
+export const Spacing = Story({
+  ...withRender(
+    () => `<div>
+    <div class="has-background-blue-1 is-flex mb-small">
+      <bal-heading level="h4" space="all">All</bal-heading>
+    </div>
+    <div class="has-background-blue-1 is-flex mb-small">
+      <bal-heading level="h4" space="none">None</bal-heading>
+    </div>
+    <div class="has-background-blue-1 is-flex mb-small">
+      <bal-heading level="h4" space="top">Top</bal-heading>
+    </div>
+    <div class="has-background-blue-1 is-flex mb-small">
+      <bal-heading level="h4" space="bottom">Bottom</bal-heading>
+    </div>
+  </div>`,
+  ),
+})
+
+export const AutoLevel = Story({
+  ...withRender(({ content, ...args }) => `<bal-heading auto-level="h5" ${props(args)}>${content}</bal-heading>`),
+  args: {
+    content:
+      'This heading is actual a H1 level, but since it requires more than one line we shrink it until it matches or is the same level as the auto-level.',
+    level: 'h1',
+    subtitle: false,
+    space: 'bottom',
+    color: '',
+    inverted: false,
+  },
+})
+
+export const CSSUtilities = Story({
+  ...withRender(
+    () => `<div>
+    <h1 class="title is-size-xxx-large">Heading 1</h1>
+    <h2 class="title is-size-xx-large">Heading 2</h2>
+    <h3 class="title is-size-x-large">Heading 3</h3>
+    <h4 class="title is-size-large">Heading 4</h4>
+    <h5 class="title is-size-normal">Heading 5</h5>
+  </div>`,
+  ),
 })
