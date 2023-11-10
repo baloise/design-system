51f38a96f979a34e615447fa0156b0f75551e0de
Mladen Planinicic <mladen.planinicic@baloise.ch>
2023-11-10 14:01:10 +0100

    chore(): update visual test

--- test/cypress/e2e/visual/bal-checkbox.visual.cy.ts
+++ test/cypress/e2e/visual/bal-checkbox.visual.cy.ts
@@ -20,12 +20,13 @@ describe('bal-checkbox', () => {
       'checkbox-long-label-select-button-checked-desktop',
       0.0,
     )
-    cy.getByTestId('select-buttons-vertical').compareSnapshot('checkbox-select-buttons-vertical')
+    cy.getByTestId('select-buttons-vertical').compareSnapshot('checkbox-select-buttons-vertical-desktop')
 
     cy.platform('tablet')
     cy.getByTestId('vertical-on-mobile').compareSnapshot('checkbox-vertical-on-mobile-tablet')
 
     cy.platform('mobile')
     cy.getByTestId('vertical-on-mobile').compareSnapshot('checkbox-vertical-on-mobile-mobile')
+    cy.getByTestId('select-buttons-vertical').compareSnapshot('checkbox-select-buttons-vertical-mobile')
   })
 })
