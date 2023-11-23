db69465b03213f6df5dbbdf8c34f6e9c71347c8d
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-11-23 09:33:35 +0100

    chore: link testing lib

--- apps/angular/v16/scripts/link.sh
+++ apps/angular/v16/scripts/link.sh
@@ -18,10 +18,15 @@ pushd packages/components
 npm link
 popd
 
+pushd packages/testing
+npm link
+popd
+
 popd
 npm link @baloise/design-system-tokens
 npm link @baloise/design-system-fonts
 npm link @baloise/design-system-css
 npm link @baloise/design-system-components
+npm link @baloise/design-system-testing
 
 echo "=> Angular project is linked with components"
