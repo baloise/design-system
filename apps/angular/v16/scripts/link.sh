7918fed367102c96595227d404a7788e5b2432c6
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-11-23 09:25:43 +0100

    chore: add missing links

--- apps/angular/v16/scripts/link.sh
+++ apps/angular/v16/scripts/link.sh
@@ -2,12 +2,26 @@
 
 pushd ../../..
 
+pushd packages/tokens
+npm link
+popd
+
+pushd packages/fonts
+npm link
+popd
+
+pushd packages/css
+npm link
+popd
+
 pushd packages/components
 npm link
-
 popd
 
 popd
+npm link @baloise/design-system-tokens
+npm link @baloise/design-system-fonts
+npm link @baloise/design-system-css
 npm link @baloise/design-system-components
 
 echo "=> Angular project is linked with components"
