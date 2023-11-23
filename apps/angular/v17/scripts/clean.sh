45fb7d30412fa30f2946c59e111efa579f1319a9
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-11-23 19:28:31 +0100

    Chore/new-output-ng-17 (#1159)
    * chore: adjust ng apps
    
    * chore: add v17
    
    * chore: update ci with v17
    
    * chore: update ci with v17
    
    * chore: update ci with v17
    
    * chore: update ci with v17
    
    * chore: update ci with v17
    
    * chore: update ci with v17
    
    * chore: update ci with v17
    
    * chore: update ci with v17
    
    * chore: update ci with v17
    
    * chore: copy proxies
    
    * chore: add missing links
    
    * chore: link testing lib
    
    * chore: link testing lib
    
    * chore: link testing lib
    
    * chore: link testing lib
    
    * chore: link testing lib
    
    * chore: link testing lib
    
    * chore: link testing lib
    
    * chore: hide icons
    
    * chore: combine links
    
    * chore: combine links
    
    * chore: add matrix
    
    * chore: add fonts
    
    * chore: add v17
    
    * chore: add v17
    
    * chore: add v17
    
    * chore: update to v17
    
    * chore: use css

---
+++ apps/angular/v17/scripts/clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -f *.tgz
+rm -f angular.json
+rm -f cypress.config.ts
+rm -f README.md
+rm -f tsconfig.app.json
+rm -f tsconfig.spec.json
+rm -f web-server.js
+
+rm -rf .angular
+rm -rf node_modules
+rm -rf dist
+rm -rf cypress
+rm -rf src
+
+echo "=> Angular project is clean"
