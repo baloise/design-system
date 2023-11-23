cc7f5cd5be3a187869f44fb85971a43b4464f7ed
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-11-23 12:31:39 +0100

    chore: combine links

--- apps/angular/v17/scripts/link.sh
+++ apps/angular/v17/scripts/link.sh
@@ -19,9 +19,9 @@ npm link
 popd
 
 popd
-npm link @baloise/design-system-tokens
-npm link @baloise/design-system-css
-npm link @baloise/design-system-components
-npm link @baloise/design-system-testing
+npm link @baloise/design-system-tokens @baloise/design-system-css @baloise/design-system-components @baloise/design-system-testing
+# npm link @baloise/design-system-css
+# npm link @baloise/design-system-components
+# npm link @baloise/design-system-testing
 
 echo "=> Angular project is linked with components"
