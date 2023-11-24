3e85de2993ad4d6bd301807ecd797967855deb07
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-11-24 15:13:31 +0100

    chore: add new component output (#1163)

--- packages/components/src/initialize.ts
+++ packages/components/src/initialize.ts
@@ -5,14 +5,20 @@ import { BalConfig, BalPlatformConfig, setupConfig } from './utils/config'
 import { VERSION } from './utils/constants/version.constant'
 import { balBrowser } from './utils/browser'
 
-export const initialize = (userConfig: BalConfig = {}, platformConfig: BalPlatformConfig = {}, win = {} as any) => {
+export const initializeBaloiseDesignSystem = (
+  userConfig: BalConfig = {},
+  platformConfig: BalPlatformConfig | undefined = undefined,
+  win = {} as any,
+) => {
   if (Object.keys(win).length === 0 && balBrowser.hasWindow) {
     win = window as any
   }
 
   win.BaloiseDesignSystem = win.BaloiseDesignSystem || {}
 
-  setPlatformHelpers(platformConfig)
+  if (platformConfig) {
+    setPlatformHelpers(platformConfig)
+  }
   setupConfig(userConfig, win)
 
   win.BaloiseDesignSystem.toastController = balToastController
