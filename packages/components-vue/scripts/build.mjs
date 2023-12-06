3fd9809b32c0adbc9df6fe57d0aeb5536214b4c8
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-12-06 20:50:19 +0100

    chore: and build script

---
+++ packages/components-vue/scripts/build.mjs
@@ -0,0 +1,17 @@
+import { done, logger, exec } from '../../../.build/utils/index.mjs'
+
+const run = async () => {
+  const log = logger('vue components build')
+  log.start()
+
+  try {
+    await exec('rollup', ['--config', 'rollup.config.js'])
+    log.succeed()
+  } catch (error) {
+    log.fail(error)
+  } finally {
+    done()
+  }
+}
+
+run()
---
+++ packages/output-targets/vue/scripts/build.mjs
@@ -0,0 +1,19 @@
+import shell from 'shelljs'
+import { exec, done, logger } from '../../../../.build/utils/index.mjs'
+
+const run = async () => {
+  const log = logger('vue output')
+  log.start()
+
+  try {
+    shell.rm('-rf', 'dist')
+    await exec('rollup', ['--config', 'rollup.config.js'])
+    log.succeed()
+  } catch (error) {
+    log.fail(error)
+  } finally {
+    done()
+  }
+}
+
+run()
