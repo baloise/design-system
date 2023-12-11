be5eb6b620906a5fbbf152576f1ffabf0d9be8d9
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-12-11 10:21:33 +0100

    fix: animation issues (#1233)

--- packages/components/src/components/bal-logo/bal-logo.animation.ts
+++ packages/components/src/components/bal-logo/bal-logo.animation.ts
@@ -1,5 +1,5 @@
-import type { AnimationItem } from 'lottie-web/build/player/lottie_svg'
-import Lottie from 'lottie-web/build/player/lottie_svg'
+import type { AnimationItem } from 'lottie-web/build/player/lottie_light_html'
+import Lottie from 'lottie-web/build/player/lottie_light_html'
 import { LogoAnimationData } from './bal-logo.data'
 
 export const animate = (el: HTMLElement, color: 'blue' | 'white'): AnimationItem => {
