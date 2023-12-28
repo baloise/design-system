4f78674ca85dc2391ae3d6e7fcf74b9711bc4452
Mladen Planinicic <mladen.planinicic@baloise.ch>
2023-12-28 11:53:38 +0100

    chore(): add label accessibility test

---
+++ test/cypress/e2e/a11y/bal-label.a11y.cy.ts
@@ -0,0 +1,31 @@
+describe('bal-label', () => {
+  context('a11y', () => {
+    beforeEach(() => cy.platform('desktop').pageA11y('/components/bal-label/test/bal-label.cy.html'))
+
+    describe('have the AA standard', () => {
+      it('basic', () => {
+        cy.getByTestId('basic').testA11y()
+      })
+      it('invalid', () => {
+        cy.getByTestId('invalid').testA11y()
+      })
+      it('disabled', () => {
+        cy.getByTestId('disabled').testA11y()
+      })
+      it('valid', () => {
+        cy.getByTestId('valid').testA11y()
+      })
+
+      testSizesA11y(['small', 'large', 'x-large', 'xx-large', 'xxx-large'])
+    })
+  })
+})
+
+function testSizesA11y(sizes: BalProps.BalLabelSize[]) {
+  for (let index = 0; index < sizes.length; index++) {
+    const size = sizes[index]
+    it(`size ${size}`, () => {
+      cy.getByTestId('basic').setProperty('size', size).testA11y()
+    })
+  }
+}
