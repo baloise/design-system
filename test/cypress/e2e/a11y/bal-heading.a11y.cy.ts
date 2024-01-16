220ce27ecb0ec20ca267314145d5c5b8412be029
Mladen Planinicic <mladen.planinicic@baloise.ch>
2024-01-16 13:53:32 +0100

    chore(): improve test

--- test/cypress/e2e/a11y/bal-heading.a11y.cy.ts
+++ test/cypress/e2e/a11y/bal-heading.a11y.cy.ts
@@ -1,30 +1,32 @@
 describe('bal-heading', () => {
   context('a11y', () => {
-    beforeEach(() => cy.platform('desktop').pageA11y('/components/bal-heading/test/bal-heading.cy.html'))
+    beforeEach(() => cy.platform('desktop').pageA11y('/components/bal-heading/test/bal-heading.a11y.html'))
 
     describe('have the AA standard', () => {
       it('basic', () => {
         cy.getByTestId('basic').testA11y()
       })
 
+      it('sizes', () => {
+        cy.getByTestId('display').testA11y()
+        cy.getByTestId('display-2').testA11y()
+        cy.getByTestId('h1').testA11y()
+        cy.getByTestId('h2').testA11y()
+        cy.getByTestId('h3').testA11y()
+        cy.getByTestId('h4').testA11y()
+        cy.getByTestId('h5').testA11y()
+
+        cy.getByTestId('xxxxx-large').testA11y()
+        cy.getByTestId('xxxx-large').testA11y()
+        cy.getByTestId('xxx-large').testA11y()
+        cy.getByTestId('xx-large').testA11y()
+        cy.getByTestId('x-large').testA11y()
+        cy.getByTestId('large').testA11y()
+        cy.getByTestId('medium').testA11y()
+        cy.getByTestId('normal').testA11y()
+      })
+
       testColorA11y(['primary', 'info', 'success', 'white', 'danger', 'blue', 'warning'])
-      testSizeA11y([
-        'display',
-        'display-2',
-        'h1',
-        'h2',
-        'h3',
-        'h4',
-        'h5',
-        'xxxxx-large',
-        'xxxx-large',
-        'xxx-large',
-        'xx-large',
-        'x-large',
-        'large',
-        'medium',
-        'normal',
-      ])
     })
   })
 })
@@ -34,15 +36,6 @@ function testColorA11y(colors: BalProps.BalHeadingColor[]) {
     const color = colors[index]
     it(`color ${color}`, () => {
       cy.getByTestId('basic').setProperty('color', color).testA11y()
-    })
-  }
-}
-
-function testSizeA11y(levels: BalProps.BalHeadingVisualLevel[]) {
-  for (let index = 0; index < levels.length; index++) {
-    const level = levels[index]
-    it(`levels ${level}`, () => {
-      cy.getByTestId('basic').setProperty('level', level).testA11y()
     })
   }
 }
