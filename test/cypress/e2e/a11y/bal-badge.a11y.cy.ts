1cdeb3e40664be8ab1eecbb9a5f26d479f9394ad
Mladen Planinicic <mladen.planinicic@baloise.ch>
2024-01-19 15:47:29 +0100

    chore(): add a11y test

---
+++ test/cypress/e2e/a11y/bal-badge.a11y.cy.ts
@@ -0,0 +1,32 @@
+describe('bal-logo', () => {
+  context('a11y', () => {
+    beforeEach(() => cy.platform('desktop').pageA11y('/components/bal-badge/test/bal-badge.a11y.html'))
+
+    describe('have the AA standard', () => {
+      it('basic', () => {
+        cy.getByTestId('basic').testA11y()
+      })
+
+      testColorA11y(['grey', 'danger', 'warning', 'success', 'red', 'yellow', 'green', 'purple'])
+      testSizeA11y(['small', 'large'])
+    })
+  })
+})
+
+function testColorA11y(colors: BalProps.BalBadgeColor[]) {
+  for (let index = 0; index < colors.length; index++) {
+    const color = colors[index]
+    it(`color ${color}`, () => {
+      cy.getByTestId('basic').setProperty('color', color).testA11y()
+    })
+  }
+}
+
+function testSizeA11y(sizes: BalProps.BalBadgeSize[]) {
+  for (let index = 0; index < sizes.length; index++) {
+    const size = sizes[index]
+    it(`size ${size}`, () => {
+      cy.getByTestId('basic').setProperty('size', size).testA11y()
+    })
+  }
+}
