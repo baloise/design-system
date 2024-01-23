b60d1957cf1e2f7461881cd73f9701ac05275d67
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2024-01-23 15:47:52 +0100

    chore: fix visual tests with attr hidden

--- test/cypress/component/bal-tabs.vue
+++ test/cypress/component/bal-tabs.vue
@@ -6,7 +6,7 @@ import { BalTabs, BalTabItem } from '../support/utils'
     <BalTabItem value="tab-a" label="Tab A">Content of Tab A</BalTabItem>
     <BalTabItem value="tab-b" label="Tab B">Content of Tab B</BalTabItem>
     <BalTabItem bubble value="tab-c" label="Tab C">Content of Tab C</BalTabItem>
-    <BalTabItem value="tab-d" label="Tab D" hidden>Hidden Content of Tab D</BalTabItem>
+    <BalTabItem value="tab-d" label="Tab D" invisible>Hidden Content of Tab D</BalTabItem>
     <BalTabItem value="tab-e" label="Tab E" disabled>Content of Tab E</BalTabItem>
     <BalTabItem value="tab-link" label="Tab link" href="https://github.com/baloise/design-system" target="_blank"
       >Content of Tab link</BalTabItem
