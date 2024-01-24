9ee3ab3f798fa20130cdad5f2186d9d633d07a0f
Mladen Planinicic <mladen.planinicic@baloise.ch>
2024-01-24 09:49:46 +0100

    chore(): rename components file

--- test/cypress/support/a11y.utils.ts
+++ test/cypress/support/a11y.utils.ts
@@ -1,4 +1,4 @@
-import * as data from '../../generated/components.json'
+import * as data from '../../generated/components-data.json'
 
 export function findPropertyValuesByTag(tag: string, propName: string) {
   const component = data.components.find(comp => comp.tag === tag)
