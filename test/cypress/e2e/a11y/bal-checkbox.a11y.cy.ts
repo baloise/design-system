fe8220f58d8691a81e6d103c554f4431357e8d19
Mladen Planinicic <mladen.planinicic@baloise.ch>
2024-01-25 16:15:05 +0100

    chore(): refactor

--- test/cypress/e2e/a11y/bal-checkbox.a11y.cy.ts
+++ test/cypress/e2e/a11y/bal-checkbox.a11y.cy.ts
@@ -3,24 +3,31 @@ describe('bal-checkbox', () => {
     beforeEach(() => cy.platform('desktop').pageA11y('/components/bal-checkbox/test/bal-checkbox.a11y.html'))
 
     describe('have the AA standard', () => {
-      it('basic', () => {
-        cy.getByTestId('basic').testA11y()
-      })
-
-      it('checked', () => {
+      it('checkbox basic', () => {
+        cy.getByTestId('checkbox-basic').testA11y()
+        cy.getByTestId('group').testA11y()
         cy.getByTestId('checked').testA11y()
-      })
-
-      it('label hidden', () => {
-        cy.getByTestId('label-hidden').testA11y()
-      })
-
-      it('disabled', () => {
+        cy.getByTestId('basic-label-hidden').testA11y()
         cy.getByTestId('disabled').testA11y()
+        cy.getByTestId('invalid').testA11y()
       })
 
-      it('invalid', () => {
-        cy.getByTestId('invalid').testA11y()
+      it('select button', () => {
+        cy.getByTestId('select-button-basic').testA11y()
+        cy.getByTestId('select-button-group').testA11y()
+        cy.getByTestId('select-button-checked').testA11y()
+        cy.getByTestId('select-button-label-hidden').testA11y()
+        cy.getByTestId('select-button-invalid').testA11y()
+        cy.getByTestId('select-button-disabled').testA11y()
+      })
+
+      it('switch basic', () => {
+        cy.getByTestId('switch-basic').testA11y()
+        cy.getByTestId('switch-group').testA11y()
+        cy.getByTestId('switch-checked').testA11y()
+        cy.getByTestId('switch-label-hidden').testA11y()
+        cy.getByTestId('switch-invalid').testA11y()
+        cy.getByTestId('switch-disabled').testA11y()
       })
     })
   })
