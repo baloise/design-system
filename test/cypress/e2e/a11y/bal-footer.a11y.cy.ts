574f98e01ae996ef495f35e53c645b73b3df21f2
Mladen Planinicic <mladen.planinicic@baloise.ch>
2024-02-06 16:02:58 +0100

    chore(): add a11y test

---
+++ test/cypress/e2e/a11y/bal-footer.a11y.cy.ts
@@ -0,0 +1,11 @@
+describe('bal-footer', () => {
+  context('a11y', () => {
+    beforeEach(() => cy.platform('desktop').pageA11y('/components/bal-footer/test/bal-footer.a11y.html'))
+
+    describe('have the AA standard', () => {
+      it('basic', () => {
+        cy.getByTestId('basic').testA11y()
+      })
+    })
+  })
+})
