81899aa011744b4a07ca5d23710db61fe1dd5cb5
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2024-02-13 11:16:17 +0100

    chore: update visual testing

--- test/cypress/e2e/a11y/bal-spinner.a11y.cy.ts
+++ test/cypress/e2e/a11y/bal-spinner.a11y.cy.ts
@@ -1,18 +1,10 @@
 describe('bal-spinner', () => {
   context('a11y', () => {
-    beforeEach(() => cy.platform('desktop').pageA11y('/components/bal-spinner/test/bal-spinner.a11y.html'))
+    beforeEach(() => cy.pageA11y('/components/bal-spinner/test/bal-spinner.a11y.html'))
 
     describe('have the AA standard', () => {
       it('basic', () => {
         cy.getByTestId('basic').testA11y()
-      })
-
-      it('small', () => {
-        cy.getByTestId('small').testA11y()
-      })
-
-      it('inverted', () => {
-        cy.getByTestId('inverted').testA11y()
       })
     })
   })
