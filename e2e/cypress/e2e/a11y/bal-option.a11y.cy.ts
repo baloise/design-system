7458e0032c2b5360e5d577b5291f490e82da7b70
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2024-03-18 11:15:46 +0100

    add new component option

---
+++ e2e/cypress/e2e/a11y/bal-option.a11y.cy.ts
@@ -0,0 +1,13 @@
+describe('bal-badge', () => {
+  context('a11y', () => {
+    beforeEach(() => cy.platform('desktop').pageA11y('/components/bal-option/test/bal-option.a11y.html'))
+
+    describe('have the AA standard', () => {
+      it('basic', () => cy.getByTestId('basic').testA11y())
+      it('selected', () => cy.getByTestId('selected').testA11y())
+      it('focused', () => cy.getByTestId('focused').testA11y())
+      it('disabled', () => cy.getByTestId('disabled').testA11y())
+      it('invalid', () => cy.getByTestId('invalid').testA11y())
+    })
+  })
+})
