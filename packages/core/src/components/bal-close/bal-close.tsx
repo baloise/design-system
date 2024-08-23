0400f90bb9ab5301e0d1f561627301e5ea5546b6
Marinkov, Magdalena <Magdalena.Marinkov@baloise.ch>
2024-08-23 16:14:05 +0200

    fix(close): make close button focusable by changing tabindex from -1 to 0

--- packages/core/src/components/bal-close/bal-close.tsx
+++ packages/core/src/components/bal-close/bal-close.tsx
@@ -53,7 +53,7 @@ export class Close implements ComponentInterface, BalConfigObserver {
           type="button"
           aria-label={label}
           title={label}
-          tabindex={0}
+          tabindex="0"
           class={{
             ...buttonEl.class(),
             ...buttonEl.modifier('inverted').class(this.inverted),
