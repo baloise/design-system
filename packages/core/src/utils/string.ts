77fd940f1b08934be8863a76a33120ea40b6759d
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2024-09-05 16:13:39 +0200

    chore: add missing unit test

--- packages/core/src/utils/string.ts
+++ packages/core/src/utils/string.ts
@@ -1,5 +1,10 @@
-export const toKebabCase = string =>
-  string
-    .replace(/([a-z])([A-Z])/g, '$1-$2')
-    .replace(/[\s_]+/g, '-')
-    .toLowerCase()
+export const toKebabCase = (string: unknown) => {
+  if (typeof string === 'string') {
+    return string
+      .replace(/([a-z])([0-9])/g, '$1-$2')
+      .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+      .replace(/[\s_]+/g, '-')
+      .toLowerCase()
+  }
+  return ''
+}
