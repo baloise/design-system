1a588309314e3b063d4a7d8712d45f8e2ec2ddd9
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2024-11-22 14:38:24 +0100

    chore: increase wait for visual

--- e2e/cypress/e2e/visual/bal-hint.visual.cy.ts
+++ e2e/cypress/e2e/visual/bal-hint.visual.cy.ts
@@ -26,7 +26,7 @@ describe('bal-hint', () => {
   })
 
   context('mobile', () => {
-    beforeEach(() => cy.platform('mobile').wait(100))
+    beforeEach(() => cy.platform('mobile').wait(200))
 
     it('basic component', () => {
       cy.getByTestId('basic').testVisual('hint-basic-mobile')
