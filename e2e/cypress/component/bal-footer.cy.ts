5f912f5b4db2a7a1bfa6aff707a43fe99a697366
Yannick Holzenkamp <yannick.holzenkamp@baloise.ch>
2024-12-05 11:06:10 +0100

    feat(bal-select): introduce overrideLinks property to customize links displayed in the footer

--- e2e/cypress/component/bal-footer.cy.ts
+++ e2e/cypress/component/bal-footer.cy.ts
@@ -14,11 +14,10 @@ describe('bal-footer', () => {
   it('should contain custom links', () => {
     cy.mount<Components.BalFooter, HTMLBalFooterElement>(`<bal-footer></bal-footer>`, {
       props: {
-        overrideLinks: [{label: 'Test Link', link: 'https://www.baloise.ch'}],
-      }
+        overrideLinks: [{ label: 'Test Link', link: 'https://www.baloise.ch' }],
+      },
     })
     cy.get('bal-footer').contains('Test Link')
     cy.get('bal-footer').should('not.contain', 'Datenschutz')
   })
-
 })
