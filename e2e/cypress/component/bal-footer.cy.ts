6ae4114e80c60f05adc1b44cbd6035dc20d7bdfa
Yannick Holzenkamp <yannick.holzenkamp@baloise.ch>
2024-12-05 11:21:57 +0100

    feat(bal-select): introduce overrideLinks property to customize links displayed in the footer

--- e2e/cypress/component/bal-footer.cy.ts
+++ e2e/cypress/component/bal-footer.cy.ts
@@ -1,16 +1,6 @@
 import { Components } from '../../generated'
 
 describe('bal-footer', () => {
-  beforeEach(() => {
-    cy.intercept('GET', 'https://www.baloise.ch/app-integration/v2/ch/footer/de.json', {
-      fixture: 'bal-footer.footer.json',
-    }).as('getFooterLinks')
-    cy.intercept('GET', 'https://www.baloise.ch/app-integration/v1/ch/socialmediachannels/de.json', {
-      fixture: 'bal-footer.socialmediachannels.json',
-    }).as('getSocialMediaChannels')
-    cy.visit('/components/bal-footer/test/bal-footer.visual.html').platform('desktop').waitForDesignSystem()
-  })
-
   it('should contain custom links', () => {
     cy.mount<Components.BalFooter, HTMLBalFooterElement>(`<bal-footer></bal-footer>`, {
       props: {
