eaa6b69af63092a2bfc90f0971c12f66bcdd3b13
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2025-02-13 22:16:06 +0100

    chore: add ci part

---
+++ packages/core/web-server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const compression = require('compression')
+const path = require('path')
+const serveIndex = require('serve-index')
+
+const app = express()
+
+app.set('port', 3333)
+
+app.use(compression())
+app.use(express.static(path.join(__dirname, 'www')))
+app.use(serveIndex(path.join(__dirname, 'www')))
+
+const server = app.listen(app.get('port'), () => {
+  console.log('The server is running on http://localhost:' + app.get('port'))
+})
+
+process.on('SIGINT', () => {
+  server.close(() => {
+    process.exit(0)
+  })
+})
