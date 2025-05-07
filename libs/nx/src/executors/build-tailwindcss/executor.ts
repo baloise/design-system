7844bb03f639615fad5f4b0e095dbc59d2b2ed32
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2025-05-07 14:49:13 +0200

    chore: add line-height and typography

--- libs/nx/src/executors/build-tailwindcss/executor.ts
+++ libs/nx/src/executors/build-tailwindcss/executor.ts
@@ -1,9 +1,11 @@
 import { mkdir, rm, writeFile } from 'fs/promises'
 import { join } from 'path'
 import { generateBorder } from './generators/border'
+import { generateTypography } from './generators/typography'
 import { NEWLINE } from './generators/utils'
 import { generateZIndex } from './generators/z-index'
 import { BuildTailwindcssExecutorSchema } from './schema'
+import { generateLineHeight } from './generators/line-height'
 
 export default async function runExecutor(options: BuildTailwindcssExecutorSchema) {
   try {
@@ -11,6 +13,8 @@ export default async function runExecutor(options: BuildTailwindcssExecutorSchem
     // content += await generateSpacing(options)
     content += await generateZIndex(options)
     content += await generateBorder(options)
+    content += await generateTypography(options)
+    content += await generateLineHeight(options)
     content += `}${NEWLINE}${NEWLINE}`
 
     console.log('content', content)
