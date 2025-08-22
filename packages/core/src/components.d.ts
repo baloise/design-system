671ff96663ba52d24f41e2daed11494c5fc49bef
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2025-08-22 11:03:19 +0200

    feat(footer): make it more themeable

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -1537,6 +1537,16 @@ export namespace Components {
          */
         "hideLinks": boolean;
         /**
+          * Image address for the logo.
+          * @default ''
+         */
+        "logoHref": string;
+        /**
+          * Svg content for the logo.
+          * @default ''
+         */
+        "logoSvg": string;
+        /**
           * If provided, the footer links will be overridden.
           * @default undefined
          */
@@ -7554,6 +7564,16 @@ declare namespace LocalJSX {
           * @default false
          */
         "hideLinks"?: boolean;
+        /**
+          * Image address for the logo.
+          * @default ''
+         */
+        "logoHref"?: string;
+        /**
+          * Svg content for the logo.
+          * @default ''
+         */
+        "logoSvg"?: string;
         /**
           * If provided, the footer links will be overridden.
           * @default undefined
