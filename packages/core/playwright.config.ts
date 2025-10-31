8f067827f0deae88f9a22df3bf038bc215982d7a
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2025-10-31 13:21:56 +0100

    chore: update ci

--- packages/core/playwright.config.ts
+++ packages/core/playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['github']],
+  reporter: [['list'], ['html'], ['github']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
