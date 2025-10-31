30138362e4d9728f77a51226f631a6ea0eb6f56f
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2025-10-31 14:22:03 +0100

    chore: add badge

---
+++ packages/core/src/components/bal-badge/test/bal-badge.visual.play.ts
@@ -0,0 +1,20 @@
+import { expect, screenshot, test } from '@baloise/ds-playwright'
+
+const TAG = 'bal-badge'
+const VARIANTS = ['basic', 'icon', 'card', 'button', 'sizes', 'colors', 'long-content']
+
+test.describe('visual', () => {
+  const image = screenshot(TAG)
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`/components/${TAG}/test/${TAG}.visual.html`)
+    await page.waitForSelector(TAG)
+  })
+
+  VARIANTS.forEach(variant => {
+    test(variant, async ({ page }) => {
+      const el = page.getByTestId(variant)
+      await expect(el).toHaveScreenshot(image(`${variant}`))
+    })
+  })
+})
