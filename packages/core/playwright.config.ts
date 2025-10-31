a267cfa050253615da92ac0d98aebb8741137ad5
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2025-10-31 12:43:51 +0100

    chore: increase workers

--- packages/core/playwright.config.ts
+++ packages/core/playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   reportSlowTests: null,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'], ['github']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
