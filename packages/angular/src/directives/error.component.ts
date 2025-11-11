c16cf43ce3ac9ccbfae8d6ed0cc6b4a884a6aa27
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2025-11-11 13:32:01 +0100

    chore(angular): remove module package (#1853)
    * chore(angular): remove module package
    
    * chore: fix directive
    
    * chore: add changeset
    
    * chore: update docs
    
    * chore: update deps for angular and remove unused

--- packages/angular/src/directives/error.component.ts
+++ packages/angular/src/directives/error.component.ts
@@ -1,7 +1,21 @@
-import { CommonModule } from '@angular/common'
-import { ChangeDetectorRef, Component, Inject, Injector } from '@angular/core'
+/* eslint-disable @angular-eslint/directive-class-suffix */
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  Directive,
+  HostBinding,
+  Inject,
+  Injector,
+  Input,
+} from '@angular/core'
+import { AbstractControl, ControlContainer, FormGroup } from '@angular/forms'
+import { BehaviorSubject } from 'rxjs'
 
-import { BalNgErrorComponent as BalNgErrorComponentBase } from '@baloise/ds-angular-common'
+import type { BaloiseDesignSystemAngularConfig } from '../utils/config'
+import { BalTokenConfig } from '../utils/token'
+import { raf } from '../utils/utils'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'bal-ng-error',
@@ -16,8 +30,102 @@ import { BalNgErrorComponent as BalNgErrorComponentBase } from '@baloise/ds-angu
     `,
   ],
 })
-export class BalNgErrorComponent extends BalNgErrorComponentBase {
-  constructor(@Inject(Injector) injector: Injector, @Inject(ChangeDetectorRef) cd: ChangeDetectorRef) {
-    super(injector, cd)
+export class BalNgErrorComponent implements AfterViewInit {
+  /**
+   * The name of form validator to show.
+   */
+  @Input() error?: string
+
+  /**
+   * FormGroup where the control of the error message is located. (optional)
+   */
+  @Input() group?: FormGroup
+
+  /**
+   * The name of the form control, which is registered in the form group.
+   */
+  @HostBinding('attr.controlname')
+  @Input()
+  controlName?: string
+
+  constructor(
+    @Inject(Injector) protected injector: Injector,
+    @Inject(ChangeDetectorRef) protected cd: ChangeDetectorRef,
+  ) {}
+
+  private controlContainer?: ControlContainer
+  private control?: AbstractControl | null
+  private config?: BaloiseDesignSystemAngularConfig
+  private invalidateOn: 'dirty' | 'touched' = 'touched'
+  ready = new BehaviorSubject(false)
+
+  ngAfterViewInit(): void {
+    this.control = this.getControl()
+
+    raf(() => this.setup())
+  }
+
+  setup = () => {
+    try {
+      this.config = this.injector.get<BaloiseDesignSystemAngularConfig>(BalTokenConfig)
+    } catch {
+      /* No config provided */
+    }
+
+    this.invalidateOn = this.config?.forms?.invalidateOn || this.invalidateOn
+
+    if (!this.control) {
+      console.warn('[BalNgErrorComponent] Could not find the given controlName in the form control container')
+    } else {
+      this.ready.next(true)
+      this.cd.detectChanges()
+    }
+  }
+
+  get hasError(): boolean {
+    if (this.controlName && this.config && this.control) {
+      if (!this.control[this.invalidateOn]) {
+        return false
+      }
+
+      if (!this.error) {
+        return this.control.invalid
+      }
+
+      if (this.control.errors) {
+        const validationErrorKeys = Object.keys(this.control.errors).filter(k => k !== 'errorType')
+        const hasValidationErrors = validationErrorKeys.length > 0
+
+        if (hasValidationErrors) {
+          return validationErrorKeys[0] === this.error // isFirstKeyOurError
+        }
+      }
+    }
+
+    return false
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private getControl(): AbstractControl<any, any> | null {
+    if (this.controlName) {
+      if (this.group) {
+        return this.group.get(this.controlName)
+      } else {
+        try {
+          this.controlContainer = this.injector.get<ControlContainer>(ControlContainer)
+        } catch {
+          /* No ControlContainer provided */
+        }
+
+        if (!this.controlContainer) {
+          return null
+        }
+
+        return this.controlContainer.control?.get(this.controlName) as AbstractControl<any, any>
+      }
+    } else {
+      console.warn('[BalNgErrorComponent] Please provide a controlName')
+    }
+    return null
   }
 }
--- test/angular/base/v20/scripts/copy.sh
+++ test/angular/base/v20/scripts/copy.sh
@@ -3,7 +3,6 @@ rm -rf src/generated
 mkdir -p src/generated
 
 # Copy angular proxy library to version space
-cp -R ../../../packages/angular-common/src src/generated/common
 cp -R ../../../packages/angular/src src/generated/angular
 
 echo "=> Angular project is copied"
