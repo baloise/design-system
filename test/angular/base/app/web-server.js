8234b8f427cd0ff0366aee023baf0af038167d62
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2025-11-13 15:34:14 +0100

    chore: adjust dist path

--- test/angular/base/app/web-server.js
+++ test/angular/base/app/web-server.js
@@ -8,8 +8,8 @@ const app = express()
 app.set('port', 4200)
 
 app.use(compression())
-app.use(express.static(path.join(__dirname, 'dist', 'browser')))
-app.use(serveIndex(path.join(__dirname, 'dist', 'browser')))
+app.use(express.static(path.join(__dirname, 'dist', 'my-app')))
+app.use(serveIndex(path.join(__dirname, 'dist', 'my-app')))
 
 const server = app.listen(app.get('port'), () => {
   console.log('The server is running on http://localhost:' + app.get('port'))
