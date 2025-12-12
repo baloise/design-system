db3bc177d32d4b08a745f5bd72d8a2980d20f841
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2025-12-12 11:49:15 +0100

    chore: fix visual heading and label

--- packages/core/config/doc-output-target/index.ts
+++ packages/core/config/doc-output-target/index.ts
@@ -134,22 +134,6 @@ export const CustomDocumentationGenerator: OutputTargetDocsCustom = {
         } catch (err) {
           console.error(err)
         }
-
-        // if (isRoot) {
-        // Testing
-        // try {
-        //   createTestingMarkdown(storyPath, component)
-        // } catch (err) {
-        //   console.error(err)
-        // }
-
-        // // Theming
-        // try {
-        //   createThemingMarkdown(storyPath, component)
-        // } catch (err) {
-        //   console.error(err)
-        // }
-        // }
       }
     }
 
@@ -174,6 +158,11 @@ export const CustomDocumentationGenerator: OutputTargetDocsCustom = {
 
         // check if file exists
         if (!existsSync(varsFilePath)) {
+          try {
+            writeFileSync(path.join(storyPath, 'theming.md'), '')
+          } catch (err) {
+            console.error(err)
+          }
           continue
         }
 
@@ -204,73 +193,5 @@ export const CustomDocumentationGenerator: OutputTargetDocsCustom = {
         sd.buildPlatform('web')
       }
     }
-
-    // const cssVarsFiles = globSync(path.join(__dirname, '../../../css/src/core/vars', '*.vars.sass'))
-
-    // const cssVars: { [key: string]: JsonDocsStyle[] } = {}
-
-    // const camelize = s => s.replace(/-./g, x => ` ${x[1].toUpperCase()}`)
-
-    // const capitalized = s => s.charAt(0).toUpperCase() + s.slice(1)
-
-    // const getFileName = (filePath: string) =>
-    //   (filePath.split(path.sep).pop() || 'global.vars.sass').replace('.vars.sass', '')
-
-    // for (let index = 0; index < cssVarsFiles.length; index++) {
-    //   const cssVarsFile = cssVarsFiles[index]
-    //   const varsFile = readFileSync(cssVarsFile, 'utf8')
-    //   const styleDocs: JsonDocsStyle[] = []
-    //   parseStyleDocs(styleDocs, varsFile)
-    //   cssVars[capitalized(camelize(getFileName(cssVarsFile)))] = styleDocs
-    // }
-
-    // let themingLines: string[] = []
-    // for (const key in cssVars) {
-    //   const table = new MarkdownTable()
-    //   table.addHeader(['Variable'])
-    //   cssVars[key].forEach(styleVariable => {
-    //     table.addRow([`\`${styleVariable.name}\``])
-    //   })
-    //   themingLines = [...themingLines, '', `### ${key}`, '', ...table.toMarkdown()]
-    // }
-
-    // const contentTheming: string[] = [
-    //   `## Global CSS Variables`,
-    //   '',
-    //   'These are the variables of the CSS-Framework which inherit from the design tokens.',
-    //   '',
-    //   ...themingLines,
-    //   '',
-    // ]
-    // mkdirSync(path.join(DOC_PATH, 'src/development/00-guides/theming'), { recursive: true })
-    // writeFileSync(path.join(DOC_PATH, 'src/development/00-guides/theming/theming.md'), contentTheming.join(NEWLINE))
-
-    /**
-     * Create contributors page
-     */
-    //   const avatar =
-    //     (contributor: any) => `<a href="${contributor.url}" target='_blank' style="flex: 1;" className="sb-unstyled text-decoration-none doc-button-card doc-shadow-hover flex flex-1 flex-direction-column bg-purple-1 p-normal radius-normal text-large text-primary"
-    // >
-    //   <span className='block text-xx-large text-align-center'>
-    //     <img src="${contributor.avatar}" alt="${contributor.name}" />
-    //   </span>
-    //   <span className='block title text-medium text-align-center mb-none text-decoration-none'>${contributor.name}</span>
-    // </a>`
-
-    //   const contributorsContent = [
-    //     `<div className="sb-unstyled flex flex-wrap gap-normal">`,
-    //     ...contributors.map(c => avatar(c)),
-    //     `</div>`,
-    //     SPACE,
-    //   ]
-
-    //   try {
-    //     writeFileSync(
-    //       path.join(DOC_PATH, 'src', 'contributing', 'contributors.md'),
-    //       contributorsContent.join(NEWLINE),
-    //     )
-    //   } catch (err) {
-    //     console.error(err)
-    //   }
   },
 }
