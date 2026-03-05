65d1671f50d0dadbee1a6b0d93bbc0ec0a530def
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2026-03-05 08:35:35 +0100

    chore: clean up

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -34,32 +34,32 @@ export { Components, JSX } from './components'
 /**
  * Types
  */
-export * from './components/bal-checkbox/bal-checkbox.type'
-export * from './components/bal-file-upload/bal-file-upload.type'
-export * from './components/bal-modal/bal-modal.type'
-export * from './components/bal-radio/bal-radio.type'
-export * from './components/bal-select/utils/bal-option.type'
-export * from './components/bal-steps/bal-step.type'
-export * from './components/bal-tabs/bal-tab.type'
+// export * from './components/bal-checkbox/bal-checkbox.type'
+// export * from './components/bal-file-upload/bal-file-upload.type'
+// export * from './components/bal-modal/bal-modal.type'
+// export * from './components/bal-radio/bal-radio.type'
+// export * from './components/bal-select/utils/bal-option.type'
+// export * from './components/bal-steps/bal-step.type'
+// export * from './components/bal-tabs/bal-tab.type'
 
 /**
  * i18n
  */
 export * from './components/bal-close/bal-close.i18n'
-export * from './components/bal-date/bal-date.i18n'
-export * from './components/bal-field/bal-field-label/bal-field-label.i18n'
-export * from './components/bal-input-stepper/bal-input-stepper.i18n'
 export * from './components/bal-label/bal-label.i18n'
-export * from './components/bal-time-input/bal-time-input.i18n'
+// export * from './components/bal-date/bal-date.i18n'
+// export * from './components/bal-field/bal-field-label/bal-field-label.i18n'
+// export * from './components/bal-input-stepper/bal-input-stepper.i18n'
+// export * from './components/bal-time-input/bal-time-input.i18n'
 
 /**
  * Utils
  */
-export { newBalCheckboxOption } from './components/bal-checkbox/utils/bal-checkbox.util'
-export { newBalRadioOption } from './components/bal-radio/utils/bal-radio.util'
-export { newBalOptionValue, newBalSingleOptionValue } from './components/bal-select/utils/bal-option.util'
-export { newBalStepOption } from './components/bal-steps/bal-step.util'
-export { newBalTabOption } from './components/bal-tabs/bal-tab.util'
+// export { newBalCheckboxOption } from './components/bal-checkbox/utils/bal-checkbox.util'
+// export { newBalRadioOption } from './components/bal-radio/utils/bal-radio.util'
+// export { newBalOptionValue, newBalSingleOptionValue } from './components/bal-select/utils/bal-option.util'
+// export { newBalStepOption } from './components/bal-steps/bal-step.util'
+// export { newBalTabOption } from './components/bal-tabs/bal-tab.util'
 export { initializeBaloiseDesignSystem as initialize, initializeBaloiseDesignSystem } from './initialize'
 export {
   BalBreakpointObserver,
@@ -72,7 +72,7 @@ export {
 export { balBrowser } from './utils/browser'
 export { BalDate } from './utils/date'
 export { balDevice, BalDevice } from './utils/device'
-export { newBalOption } from './utils/dropdown/option'
+// export { newBalOption } from './utils/dropdown/option'
 export { scrollToFirstInvalidField } from './utils/form'
 export {
   componentOnReady,
@@ -88,7 +88,7 @@ export {
   waitForDesignSystem,
   waitOnLoadEventCallback,
 } from './utils/helpers'
-export { INVALID_VALUE } from './utils/mask/mask-util'
+// export { INVALID_VALUE } from './utils/mask/mask-util'
 export {
   BalOrientationInfo,
   BalOrientationObserver,
@@ -101,23 +101,5 @@ export { BalSwipeInfo, BalSwipeObserver, BalSwipeSubject } from './utils/swipe'
 /**
  * Controllers
  */
-export * from './components/bal-modal/bal-modal.controller'
+// export * from './components/bal-modal/bal-modal.controller'
 export * from './components/bal-notification/bal-notification.controller'
-
-/**
- * Events
- */
-
-/**
- * deprecated
- */
-export {
-  getPlatforms,
-  hasTouchSupport,
-  isBrowser,
-  isPlatform,
-  NewBalOptionValue,
-  NewBalSingleOptionValue,
-  Platforms,
-  PlatformSrcSet,
-} from './utils/legacy'
--- packages/core/src/utils/mask/context/mask-mouse-context.ts
+++
@@ -1,7 +0,0 @@
-import { MaskContext } from './mask-context'
-import { MaskContextEvent } from './mask-context-interfaces'
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface MaskMouseContextEvent extends MaskContextEvent {}
-
-export class MaskMouseContext extends MaskContext<MaskMouseContextEvent> {}
