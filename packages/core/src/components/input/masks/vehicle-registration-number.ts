e2eedd0ea46b08ae53e924fd8bf024685f2f227a
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2026-04-15 15:50:45 +0200

    feat: implement VehicleRegistrationNumber mask formatter

--- packages/core/src/components/input/masks/vehicle-registration-number.ts
+++ packages/core/src/components/input/masks/vehicle-registration-number.ts
@@ -1,18 +1,36 @@
+import { isCtrlOrCommandKey, ACTION_KEYS, NUMBER_KEYS } from '../../../utils/constants/keys.constant'
+import { stopEventBubbling } from '../../../utils/form-control'
+import { formatVehicleRegistrationNumber, MAX_LENGTH_VEHICLE_REGISTRATION_NUMBER } from '../input-util'
 import { InputMaskFormatterInterface } from '../input.mask'
 
 export class VehicleRegistrationNumber implements InputMaskFormatterInterface {
   name: DS.InputMask = 'vehicle-registration-number'
-  maxLength = 2
+  maxLength = MAX_LENGTH_VEHICLE_REGISTRATION_NUMBER
 
   format(value: string | null): string | null {
-    throw new Error('Method not implemented.')
+    if (!value) return value
+    return formatVehicleRegistrationNumber(value)
   }
 
   onInput(ev: InputEvent): void {
-    throw new Error('Method not implemented.')
+    const input = ev.target as HTMLInputElement | null
+    if (!input) return
+
+    const cursorStart = (ev.target as HTMLInputElement).selectionStart ?? 0
+    const cursorEnd = (ev.target as HTMLInputElement).selectionEnd ?? 0
+
+    const raw = input.value.replace(/\D/g, '').substring(0, this.maxLength)
+    input.value = formatVehicleRegistrationNumber(raw)
+
+    if (cursorStart < raw.length) {
+      input.setSelectionRange(cursorStart, cursorEnd)
+    }
   }
 
-  onKeydown(ev: InputEvent): void {
-    throw new Error('Method not implemented.')
+  onKeydown(ev: KeyboardEvent): void {
+    if (isCtrlOrCommandKey(ev)) return
+    if (![...NUMBER_KEYS, ...ACTION_KEYS].includes(ev.key)) {
+      stopEventBubbling(ev)
+    }
   }
 }
