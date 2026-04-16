404144408e3d929f8841243d3b0a885902ac5917
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2026-04-16 22:47:40 +0200

    chore: migrate number-input

--- packages/core/src/components/number-input/test/number-input.visual.play.ts
+++ packages/core/src/components/number-input/test/number-input.visual.play.ts
@@ -5,19 +5,6 @@ const VARIANTS = ['basic', 'disabled', 'invalid', 'valid', 'warning', 'decimal',
 
 const image = screenshot(TAG)
 
-test.describe('style', () => {
-  test.beforeEach('Setup', async ({ page }) => {
-    await page.setupVisualTest(`/components/${TAG}/test/${TAG}.style.html`)
-  })
-
-  VARIANTS.filter(v => !['decimal', 'suffix'].includes(v)).forEach(variant => {
-    test(variant, async ({ page }) => {
-      const el = page.getByTestId(variant)
-      await expectScreenshot(el, image(`style-${variant}`))
-    })
-  })
-})
-
 test.describe('host', () => {
   test.beforeEach('Setup', async ({ page }) => {
     await page.setupVisualTest(`/components/${TAG}/test/${TAG}.visual.html`)
