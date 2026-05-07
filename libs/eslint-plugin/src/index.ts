dcafb296cba9c023378a5b958e3d3bd17a563c28
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2026-05-07 15:04:03 +0200

    feat(eslint-plugin): update to flat config format with meta field

--- libs/eslint-plugin/src/index.ts
+++ libs/eslint-plugin/src/index.ts
@@ -10,6 +10,7 @@ import { methodPrivate } from './rules/method-private'
 import { componentTagPrefix } from './rules/component-tag-prefix'
 
 const plugin = {
+  meta: { name: '@baloise/ds', version: '0.0.1' },
   rules: {
     'no-relative-imports': noRelativeImports,
     'prop-readonly': propReadonly,
@@ -25,20 +26,22 @@ const plugin = {
   configs: {} as Record<string, unknown>,
 }
 
-plugin.configs['recommended'] = {
-  plugins: ['@baloise/ds'],
-  rules: {
-    '@baloise/ds/no-relative-imports': 'error',
-    '@baloise/ds/prop-readonly': 'error',
-    '@baloise/ds/prop-type-annotation': 'error',
-    '@baloise/ds/listen-naming': 'error',
-    '@baloise/ds/watch-naming': 'error',
-    '@baloise/ds/handler-naming': 'warn',
-    '@baloise/ds/event-prefix': 'error',
-    '@baloise/ds/method-async': 'error',
-    '@baloise/ds/method-private': 'warn',
-    '@baloise/ds/component-tag-prefix': 'error',
+plugin.configs['recommended'] = [
+  {
+    plugins: { '@baloise/ds': plugin },
+    rules: {
+      '@baloise/ds/no-relative-imports': 'error',
+      '@baloise/ds/prop-readonly': 'error',
+      '@baloise/ds/prop-type-annotation': 'error',
+      '@baloise/ds/listen-naming': 'error',
+      '@baloise/ds/watch-naming': 'error',
+      '@baloise/ds/handler-naming': 'warn',
+      '@baloise/ds/event-prefix': 'error',
+      '@baloise/ds/method-async': 'error',
+      '@baloise/ds/method-private': 'warn',
+      '@baloise/ds/component-tag-prefix': 'error',
+    },
   },
-}
+]
 
 export = plugin
