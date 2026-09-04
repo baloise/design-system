db01c241fc659b2294733c277ed8cc75dd6c0741
Gery Hirschfeld <gerhard.hirschfeld@helvetia.ch>
2026-09-04 10:50:06 +0200

    chore: base => default

--- packages/core/src/components/divider/divider.tsx
+++ packages/core/src/components/divider/divider.tsx
@@ -56,7 +56,7 @@ export class Divider implements DsComponentInterface {
    */
   @Prop()
   @OneOf(DIVIDER_COLORS)
-  readonly color: DividerColor = 'base'
+  readonly color: DividerColor = 'default'
 
   /**
    * Defines if the separator line is dashed or solid. Default is solid.
